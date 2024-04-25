99002b670fe4fa4b4f9ca7de00658838297cd410
Wojciech Lesniak <wojciech.lesniak@fenergo.com>
2024-04-25 17:17:29 +0200

    Update

--- talkietalkie/TalkieTalkie/app/build.gradle.kts
+++ talkietalkie/TalkieTalkie/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.compose.ui:ui-android:1.6.3")
+    implementation("androidx.compose.ui:ui-android:1.6.6")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.google.android.gms:play-services-ads:23.0.0")
     testImplementation("junit:junit:4.13.2")
@@ -93,7 +93,7 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class")
 
     // Optional - Integration with activities
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.9.0")
     // Optional - Integration with ViewModels
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     // Optional - Integration with LiveData
