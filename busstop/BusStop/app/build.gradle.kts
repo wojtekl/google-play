62a0640a35db3edbc60ee7b2f2742c562f916b1a
Wojciech Lesniak <wojciech.lesniak@fenergo.com>
2024-05-01 14:57:52 +0200

    Update

--- busstop/BusStop/app/build.gradle.kts
+++ busstop/BusStop/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "github.wleap.busstop"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.1.0"
+        versionCode = 13
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
