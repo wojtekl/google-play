244f974966de6483930ae1bf5bf12c3eec781499
Wojciech Lesniak <wojciech.lesniak@fenergo.com>
2024-05-25 19:59:21 +0200

    Update

--- talkietalkie/TalkieTalkie/app/build.gradle.kts
+++ talkietalkie/TalkieTalkie/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "github.wleap.talkietalkie.admob"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.2.0"
+        versionCode = 8
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
