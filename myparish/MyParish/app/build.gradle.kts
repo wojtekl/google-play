a7c815efafcb0d86f4eb4f6316cbcedd84c55f81
wojtekl <lesniakwojciech@gmail.com>
2024-09-04 16:58:49 +0200

    Update

--- myparish/MyParish/app/build.gradle.kts
+++ myparish/MyParish/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "github.wleap.myparish"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.1.1"
+        versionCode = 11
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
