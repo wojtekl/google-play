de0e92d08b3d8c5fb5c0c9099a44f94401939734
wojtekl <lesniakwojciech@gmail.com>
2024-10-07 17:40:23 +0200

    update

--- shoppinglist/webapp/repository.php
+++ shoppinglist/webapp/repository.php
@@ -35,7 +35,7 @@ class Repository {
   }
   
   public function getCenyAll($kraj) {
-    return $this -> execute("SELECT `s`.`PRODUKT`, `s`.`SKLEP`, `s`.`CENA`, `s`.`DODANO` FROM `CENA` `s` JOIN (SELECT `d`.`PRODUKT`, `d`.`SKLEP`, `d`.`CENA`, MAX(`d`.`DODANO`) AS `DODANO` FROM `CENA` `d` JOIN (SELECT `PRODUKT`, `SKLEP`, MIN(`CENA`) AS `CENA` FROM `CENA` WHERE `KRAJ` = '$kraj' AND `DODANO` > CURRENT_TIMESTAMP - INTERVAL 2 DAY GROUP BY `PRODUKT`) `c` ON `c`.`PRODUKT` = `d`.`PRODUKT` AND `c`.`CENA` = `d`.`CENA` GROUP BY `PRODUKT`, `CENA`) `cd` ON `cd`.`PRODUKT` = `s`.`PRODUKT` AND `cd`.`CENA` = `s`.`CENA` AND `cd`.`DODANO` = `s`.`DODANO` UNION SELECT `PRODUKT`, `SKLEP`, `CENA`, MAX(`DODANO`) AS `DODANO` FROM `CENA` WHERE `KRAJ` = '$kraj' GROUP BY `PRODUKT` HAVING `DODANO` < CURRENT_TIMESTAMP - INTERVAL 2 DAY ORDER BY `PRODUKT`;");
+    return $this -> execute("SELECT `s`.`PRODUKT`, `s`.`SKLEP`, `s`.`CENA`, `s`.`DODANO` FROM `CENA` `s` JOIN (SELECT `d`.`PRODUKT`, `d`.`SKLEP`, `d`.`CENA`, MAX(`d`.`DODANO`) AS `DODANO` FROM `CENA` `d` JOIN (SELECT `PRODUKT`, `SKLEP`, MIN(`CENA`) AS `CENA` FROM `CENA` WHERE `KRAJ` = '$kraj' AND `DODANO` > CURRENT_TIMESTAMP - INTERVAL 2 DAY GROUP BY `PRODUKT`) `c` ON `c`.`PRODUKT` = `d`.`PRODUKT` AND `c`.`CENA` = `d`.`CENA` GROUP BY `PRODUKT`, `CENA`) `cd` ON `cd`.`PRODUKT` = `s`.`PRODUKT` AND `cd`.`CENA` = `s`.`CENA` AND `cd`.`DODANO` = `s`.`DODANO` UNION SELECT `PRODUKT`, `SKLEP`, `CENA`, MAX(`DODANO`) AS `DODANO` FROM `CENA` WHERE `KRAJ` = '$kraj' GROUP BY `PRODUKT` HAVING `DODANO` < CURRENT_TIMESTAMP - INTERVAL 2 DAY ORDER BY `DODANO` DESC `SKLEP`;");
   }
 
   public function getUpdate($timestamp, $kraj) {
