7795666a6c827a893d630df774ed557ae868f4bf
wojtekl <lesniakwojciech@gmail.com>
2024-10-07 16:35:25 +0200

    update

--- shoppinglist/webapp/App.jsx
+++ shoppinglist/webapp/App.jsx
@@ -3,10 +3,19 @@ class App extends React.Component {
     super(props);
     
     this.state = {
-      source: (
-<List properties={["produkt", "sklep", "cena", "dodano"]} list={[]} expandable={true} replace={this.replace} back={this.back} />
-      )
+      source: null
     }
+  }
+
+  componentWillMount() {
+    let self = this;
+      let kraj = new URLSearchParams(new URL(window.location).search).get("lang");
+      if (null == kraj) kraj = navigator.language;
+      if (kraj.startsWith("pl")) kraj = "pl";
+      axios.post("http://zakupy.ugu.pl/produkty" + "?lang=pl", { lang: 'test' }).then(function (response) {
+        console.log(response)
+        self.replace(<List properties={["produkt", "sklep", "cena", "dodano"]} list={response.data} expandable={true} replace={self.replace} back={self.back} />);
+      })
   }
   
   replace = (source) => {
@@ -18,14 +27,6 @@ class App extends React.Component {
   }
   
   render() {
-    let self = this;
-      let kraj = new URLSearchParams(new URL(window.location).search).get("lang");
-      if (null == kraj) kraj = navigator.language;
-      if (kraj.startsWith("pl")) kraj = "pl";
-      axios.post("http://zakupy.ugu.pl/produkty" + "?lang=pl", { lang: 'test' }).then(function (response) {
-        console.log(response)
-        self.replace(<List properties={["produkt", "sklep", "cena", "dodano"]} list={response.data} expandable={true} replace={self.replace} back={self.back} />);
-      })
     return this.state.source
   }
 }
