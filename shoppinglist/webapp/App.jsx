44f727fec440304cc8c64909a59f0ec391dcee03
wleap <wojtekl@users.noreply.github.com>
2024-10-12 13:05:06 +0200

    Update App.jsx

--- shoppinglist/webapp/App.jsx
+++ shoppinglist/webapp/App.jsx
@@ -9,9 +9,7 @@ class App extends React.Component {
         {localise.loading}
       </button>
           <script type="text/javascript">
-            function red () {
-              window.location.href = "http://rawcdn.githack.com/wojtekl/google-play/3f16147e64cf834b70f8b0a70cd3aad5e7e6feaa/shoppinglist/webapp/index.html?fbclid=IwY2xjawF3G6hleHRuA2FlbQIxMAABHbb0eyYlxn8PpEibaMxaZVk4fJ1FzbIqmv0-CkhqapnjRZQC1BYz7dHjwQ_aem_9zypv7gNAgvdFcQ80ntgLg";
-            }
+            
           </script>
           <p>Nie widzisz cen? Kliknij <a href="#app" onclick="red();">tutaj</a></p></div>
       )
