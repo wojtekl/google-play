3c575b877d40543a7fc49be3afcfb94c6d605eaa
wleap <wojtekl@users.noreply.github.com>
2024-10-16 11:29:02 +0200

    Update List.jsx

--- shoppinglist/webapp/List.jsx
+++ shoppinglist/webapp/List.jsx
@@ -31,7 +31,7 @@ class List extends React.Component {
             </li>
           </ul>
           <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </form>
         </div>
