0b85db54eb787e376fa999120f9ee32a6c2ec0b4
wojtekl <lesniakwojciech@gmail.com>
2024-11-17 05:45:28 +0100

    update

--- shoppinglist/webapp/List.jsx
+++ shoppinglist/webapp/List.jsx
@@ -37,7 +37,7 @@ class List extends React.Component {
             </li>
           </ul>
           {!this.props.selected && <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder={localise.search} aria-label="Search" onKeyUp={this.handleFilter} />
+    <input class="form-control mr-sm-2" type="search" placeholder={localise.search} aria-label="Search" onKeyUp={this.handleFilter} onSearch={this.handleFilter} />
   </form>}
         </div>
         <div class="row mt-3">
