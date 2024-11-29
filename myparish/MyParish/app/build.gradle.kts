a4d70508fd157b518fe7f14abe232e5d0ddee8dd
wojtekl <lesniakwojciech@gmail.com>
2024-11-29 16:22:26 +0100

    update

--- myparish/MyParish/app/build.gradle.kts
+++ myparish/MyParish/app/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 
 android {
     namespace = "github.wleap.myparish"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "github.wleap.myparish"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 15
-        versionName = "1.1.6"
+        targetSdk = 35
+        versionCode = 16
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -50,14 +50,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
@@ -99,7 +99,7 @@ dependencies {
     // Optional - Integration with activities
     implementation("androidx.activity:activity-compose:1.9.3")
     // Optional - Integration with ViewModels
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     // Optional - Integration with LiveData
     implementation("androidx.compose.runtime:runtime-livedata")
     // Optional - Integration with RxJava
@@ -137,12 +137,12 @@ dependencies {
     // endregion
 
     // region Admob
-    implementation("com.google.android.gms:play-services-ads:23.4.0")
-    implementation("com.google.android.ump:user-messaging-platform:3.0.0")
+    implementation("com.google.android.gms:play-services-ads:23.5.0")
+    implementation("com.google.android.ump:user-messaging-platform:3.1.0")
     // endregion
 
     // region App Open
-    val lifecycle_version = "2.8.6"
+    val lifecycle_version = "2.8.7"
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
     kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
