1de4db816ab594d05daaf6a836c0a16bd73f95f2
wojtekl <lesniakwojciech@gmail.com>
2024-12-02 19:34:43 +0100

    update

--- shoppinglist/webapp/List.jsx
+++ shoppinglist/webapp/List.jsx
@@ -22,8 +22,8 @@ class List extends React.Component {
 
   handleChange = (event) => {
     console.log(this.props.list);
-    const selected = !this.props.selected ? this.state.selected : this.props.selected;
-    store.dispatch({type: event.target.checked ? 'selected/added' : 'selected/removed', payload: this.props.list.find(i => i.produkt === selected).id});
+    const selected = !this.props.selected ? this.props.list.find(i => i.produkt === this.props.selected).id : this.state.selected;
+    store.dispatch({type: event.target.checked ? 'selected/added' : 'selected/removed', payload: selected});
   }
 
   handleCopy = () => {
