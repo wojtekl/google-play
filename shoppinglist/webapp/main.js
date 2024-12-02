4bb287d6350f113ed95672ce73e491759709f91a
wojtekl <lesniakwojciech@gmail.com>
2024-12-02 14:10:17 +0100

    update

--- shoppinglist/webapp/main.js
+++ shoppinglist/webapp/main.js
@@ -16,7 +16,7 @@ const selectedReducer = (state = this.initialState, action) => {
   }
 }
 
-const store = ReactRedux.createStoreHook(selectedReducer);
+var store = ReactRedux.createStoreHook(selectedReducer);
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
