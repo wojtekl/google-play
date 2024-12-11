7b134b1cfce1b8972f3ed3d9f27158d6a43cf351
wojtekl <lesniakwojciech@gmail.com>
2024-12-11 20:45:20 +0100

    update

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -2,21 +2,36 @@ class App extends React.Component {
   render() {
     return (<Carousel>
       <Carousel.Item>
+        <img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel1.jpg"
+          class="d-block w-100" alt="..." />
         <Carousel.Caption>
-          <h3>First</h3>
-          <p>sub</p>
+          <h1>Aplikacje</h1>
+          <p>mobilne i internetowe</p>
+          <p>
+            <a class="btn btn-lg btn-primary" href="#oferta">Przejdź</a>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel2.jpg"
+          class="d-block w-100" alt="..." />
         <Carousel.Caption>
-          <h3>Second</h3>
-          <p>sub</p>
+          <h1>Produkty</h1>
+          <p>Zobacz zrealizowane projekty.</p>
+          <p>
+            <a class="btn btn-lg btn-primary" href="#produkty">Przejdź</a>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+        <img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel3.jpg"
+          class="d-block w-100" alt="..." />
         <Carousel.Caption>
-          <h3>Third</h3>
-          <p>sub</p>
+          <h1>Kontakt</h1>
+          <p>Dowiedz się jak złożyć zamówienie.</p>
+          <p>
+            <a class="btn btn-lg btn-primary" href="#kontakt">Przejdź</a>
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>)
