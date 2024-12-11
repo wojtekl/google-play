820471cf2a3f013c07e89200fff1fadc536a0564
wojtekl <lesniakwojciech@gmail.com>
2024-12-11 20:53:28 +0100

    update

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -2,36 +2,27 @@ class App extends React.Component {
   render() {
     return (<Carousel>
       <Carousel.Item>
-        <img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel1.jpg"
-          class="d-block w-100" alt="..." />
+        <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel1.jpg" />
         <Carousel.Caption>
           <h1>Aplikacje</h1>
           <p>mobilne i internetowe</p>
-          <p>
-            <a class="btn btn-lg btn-primary" href="#oferta">Przejdź</a>
-          </p>
+          <Button href="#oferta" variant="primary" size="lg">Przejdź</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel2.jpg"
-          class="d-block w-100" alt="..." />
+        <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel2.jpg" />
         <Carousel.Caption>
           <h1>Produkty</h1>
           <p>Zobacz zrealizowane projekty.</p>
-          <p>
-            <a class="btn btn-lg btn-primary" href="#produkty">Przejdź</a>
-          </p>
+          <Button href="#produkty" variant="primary" size="lg">Przejdź</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel3.jpg"
-          class="d-block w-100" alt="..." />
+        <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel3.jpg" />
         <Carousel.Caption>
           <h1>Kontakt</h1>
           <p>Dowiedz się jak złożyć zamówienie.</p>
-          <p>
-            <a class="btn btn-lg btn-primary" href="#kontakt">Przejdź</a>
-          </p>
+          <Button href="#kontakt" variant="primary" size="lg">Przejdź</Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>)
--- webapp/main.jsx
+++ webapp/main.jsx
@@ -1,3 +1,5 @@
+const Button = ReactBootstrap.Button;
 const Carousel = ReactBootstrap.Carousel;
+const Image = ReactBootstrap.Image;
 
 ReactDOM.render(<App />, document.getElementById('app'));
