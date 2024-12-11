ecf7ec33cfba546048beff5bc01ac8656e15316f
wojtekl <lesniakwojciech@gmail.com>
2024-12-11 21:43:19 +0100

    update

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
         <Carousel.Caption>
           <h1>Aplikacje</h1>
           <p>mobilne i internetowe</p>
-          <Button href="#oferta" variant="primary" size="lg">Przejdź</Button>
+          <Button href="#offer" variant="primary" size="lg">Przejdź</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -14,7 +14,7 @@ class App extends React.Component {
         <Carousel.Caption>
           <h1>Produkty</h1>
           <p>Zobacz zrealizowane projekty.</p>
-          <Button href="#produkty" variant="primary" size="lg">Przejdź</Button>
+          <Button href="#products" variant="primary" size="lg">Przejdź</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -22,53 +22,50 @@ class App extends React.Component {
         <Carousel.Caption>
           <h1>Kontakt</h1>
           <p>Dowiedz się jak złożyć zamówienie.</p>
-          <Button href="#kontakt" variant="primary" size="lg">Przejdź</Button>
+          <Button href="#contact" variant="primary" size="lg">Przejdź</Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
       <Container className="marketing">
-        <Row className="featurette" controlId="oferta">
+        <Row id="offer" className="featurette">
           <Col md="7">
             <h2 class="featurette-heading fw-normal lh-1">Oferta</h2>
             <p class="lead">
-              <ul>
-                <li>Aplikacje mobilne i internetowe</li>
-                <li>Rozszerzenia do Office, Chrome, Visual Studio Code itp</li>
-                <li>Zabezpieczanie połączeń sieciowych</li>
-                <li>Witryny internetowe</li>
-                <li>Rozkłady jazdy</li>
-                <li>Domowe sieci komputerowe</li>
-                <li>Testowanie rozwiązań informatycznych</li>
-              </ul>
+              <ListGroup as="ul">
+                <ListGroup.Item>Aplikacje mobilne i internetowe</ListGroup.Item>
+                <ListGroup.Item>Rozszerzenia do Office, Chrome, Visual Studio Code itp</ListGroup.Item>
+                <ListGroup.Item>Zabezpieczanie połączeń sieciowych</ListGroup.Item>
+                <ListGroup.Item>Witryny internetowe</ListGroup.Item>
+                <ListGroup.Item>Rozkłady jazdy</ListGroup.Item>
+                <ListGroup.Item>Domowe sieci komputerowe</ListGroup.Item>
+                <ListGroup.Item>Testowanie rozwiązań informatycznych</ListGroup.Item>
+              </ListGroup>
             </p>
           </Col>
           <Col md="5"></Col>
         </Row>
         <hr class="featurette-divider" />
-        <Row className="featurette" controlId="produkty">
+        <Row id="products" className="featurette">
           <Col lg="4">
-            <Image
-              src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/pricey/Pricey/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
+            <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/pricey/Pricey/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
             <h2 class="fw-normal">Pricey</h2>
             <p>Porównywarka cenowa z historią</p>
             <Button href="https://pricey.wuaze.com" variant="secondary">Uruchom</Button>
           </Col>
           <Col lg="4">
-            <Image
-              src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/busstop/BusStop/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
+            <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/busstop/BusStop/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
             <h2 class="fw-normal">BusStop</h2>
             <p>Rozkład jazdy autobusów z mapą</p>
             <Button href="https://play.google.com/store/apps/details?id=github.wleap.busstop" variant="secondary">Zainstaluj</Button>
           </Col>
           <Col lg="4">
-            <Image
-              src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MyParish/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
+            <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MyParish/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
             <h2 class="fw-normal">Moja Parafia</h2>
             <p>Katalog stron parafialnych z mapą</p>
             <Button href="https://play.google.com/store/apps/details?id=github.wleap.myparish" variant="secondary">Zainstaluj</Button>
           </Col>
         </Row>
-        <Row className="featurette" controlId="kontakt">
+        <Row id="contact" className="featurette">
           <Col md="5">
             <Image src="https://github.com/wojtekl/google-play/blob/main/barcode.png?raw=true" />
           </Col>
--- webapp/main.jsx
+++ webapp/main.jsx
@@ -3,6 +3,7 @@ const Carousel = ReactBootstrap.Carousel;
 const Col = ReactBootstrap.Col;
 const Container = ReactBootstrap.Container;
 const Image = ReactBootstrap.Image;
+const ListGroup = ReactBootstrap.ListGroup;
 const Row = ReactBootstrap.Row;
 
 ReactDOM.render(<App />, document.getElementById('app'));
