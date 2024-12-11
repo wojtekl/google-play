3d3f39d8dcb6cc0e317086cf0e4bf7cdbd2b773c
wojtekl <lesniakwojciech@gmail.com>
2024-12-11 21:33:59 +0100

    update

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -26,25 +26,59 @@ class App extends React.Component {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    <Container className="marketing">
-      <Row className="featurette">
-        <Col md="7">
-        <h2 class="featurette-heading fw-normal lh-1">Oferta</h2>
-        <p class="lead">
-        <ul>
-          <li>Aplikacje mobilne i internetowe</li>
-          <li>Rozszerzenia do Office, Chrome, Visual Studio Code itp</li>
-          <li>Zabezpieczanie połączeń sieciowych</li>
-          <li>Witryny internetowe</li>
-          <li>Rozkłady jazdy</li>
-          <li>Domowe sieci komputerowe</li>
-          <li>Testowanie rozwiązań informatycznych</li>
-        </ul>
-        </p>
-        </Col>
-        <Col md="5"></Col>
-      </Row>
-    </Container>
+      <Container className="marketing">
+        <Row className="featurette" controlId="oferta">
+          <Col md="7">
+            <h2 class="featurette-heading fw-normal lh-1">Oferta</h2>
+            <p class="lead">
+              <ul>
+                <li>Aplikacje mobilne i internetowe</li>
+                <li>Rozszerzenia do Office, Chrome, Visual Studio Code itp</li>
+                <li>Zabezpieczanie połączeń sieciowych</li>
+                <li>Witryny internetowe</li>
+                <li>Rozkłady jazdy</li>
+                <li>Domowe sieci komputerowe</li>
+                <li>Testowanie rozwiązań informatycznych</li>
+              </ul>
+            </p>
+          </Col>
+          <Col md="5"></Col>
+        </Row>
+        <hr class="featurette-divider" />
+        <Row className="featurette" controlId="produkty">
+          <Col lg="4">
+            <Image
+              src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/pricey/Pricey/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
+            <h2 class="fw-normal">Pricey</h2>
+            <p>Porównywarka cenowa z historią</p>
+            <Button href="https://pricey.wuaze.com" variant="secondary">Uruchom</Button>
+          </Col>
+          <Col lg="4">
+            <Image
+              src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/busstop/BusStop/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
+            <h2 class="fw-normal">BusStop</h2>
+            <p>Rozkład jazdy autobusów z mapą</p>
+            <Button href="https://play.google.com/store/apps/details?id=github.wleap.busstop" variant="secondary">Zainstaluj</Button>
+          </Col>
+          <Col lg="4">
+            <Image
+              src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MyParish/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp" />
+            <h2 class="fw-normal">Moja Parafia</h2>
+            <p>Katalog stron parafialnych z mapą</p>
+            <Button href="https://play.google.com/store/apps/details?id=github.wleap.myparish" variant="secondary">Zainstaluj</Button>
+          </Col>
+        </Row>
+        <Row className="featurette" controlId="kontakt">
+          <Col md="5">
+            <Image src="https://github.com/wojtekl/google-play/blob/main/barcode.png?raw=true" />
+          </Col>
+          <Col md="7">
+            <h2 class="featurette-heading fw-normal lh-1">Kontakt</h2>
+            <p class="lead">Pytania oraz zamówienia proszę kierować na adres e-mail:</p>
+            <p>wleap.zhulp@slmails.com</p>
+          </Col>
+        </Row>
+      </Container>
     </div>)
   }
 }
