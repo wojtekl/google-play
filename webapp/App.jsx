c2023aa1f3c029e57fd2c975392e485233fcc1b5
wojtekl <lesniakwojciech@gmail.com>
2024-12-13 15:00:49 +0100

    update

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -8,25 +8,25 @@ class AppInner extends React.Component {
       <Carousel.Item>
         <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel1.jpg" fluid />
         <Carousel.Caption>
-          <h1>Aplikacje {t('pow')}</h1>
-          <p>mobilne i internetowe </p>
-          <Button href="#/offer" variant="primary" size="lg">Przejdź</Button>
+          <h1>{t('title_apps')}</h1>
+          <p>{t('subtitle_apps')}</p>
+          <Button href="#/offer" variant="primary" size="lg">{t('button_see')}</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel2.jpg" fluid />
         <Carousel.Caption>
-          <h1>Produkty</h1>
-          <p>Zobacz zrealizowane projekty.</p>
-          <Button href="#/products" variant="primary" size="lg">Przejdź</Button>
+          <h1>{t('title_products')}</h1>
+          <p>{t('subtitle_products')}</p>
+          <Button href="#/products" variant="primary" size="lg">{t('button_see')}</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel3.jpg" fluid />
         <Carousel.Caption>
-          <h1>Kontakt</h1>
-          <p>Dowiedz się jak złożyć zamówienie.</p>
-          <Button href="#/contact" variant="primary" size="lg">Przejdź</Button>
+          <h1>{t('title_contact')}</h1>
+          <p>{t('subtitle_contact')}</p>
+          <Button href="#/contact" variant="primary" size="lg">{t('button_see')}</Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
--- webapp/i18next-resources.jsx
+++ webapp/i18next-resources.jsx
@@ -1,12 +1,24 @@
 const resources = {
   en: {
     translation: {
-      "pow": "wojtek hey"
+      "button_see": "See",
+      "title_apps": "Apps",
+      "title_products": "Products",
+      "title_contact": "Contact",
+      "subtitle_apps": "mobile and web",
+      "subtitle_products": "Implementations",
+      "subtitle_contact": "How to order"
     }
   },
   pl: {
     translation: {
-      "pow": "wojtek hej"
+      "button_see": "Zobacz",
+      "title_apps": "Aplikacje",
+      "title_products": "Produkty",
+      "title_contact": "Kontakt",
+      "subtitle_apps": "mobilne i internetowe",
+      "subtitle_products": "Zrealizowane projekty",
+      "subtitle_contact": "Jak złożyć zamówienie"
     }
   }
 }
