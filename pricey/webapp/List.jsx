fe9ecf6e495b9b5b0c8e1efb47e58717f2e82467
wojtekl <lesniakwojciech@gmail.com>
2024-12-13 17:14:54 +0100

    update

--- pricey/webapp/List.jsx
+++ pricey/webapp/List.jsx
@@ -36,7 +36,7 @@ class List extends React.Component {
         <Row className="mt-3">
           <Nav>
             <Nav.Item>
-              <Button href="#" data-toggle="modal" data-target="#exampleModal">{!this.props.selected ? localise.newProduct : localise.updatePrice}</Button>
+              <a class="nav-link active" href="#" data-toggle="modal" data-target="#exampleModal">{!this.props.selected ? localise.newProduct : localise.updatePrice}</a>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link href="https://rb.gy/sqezhd">{localise.getTheApp}</Nav.Link>
