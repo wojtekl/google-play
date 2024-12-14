3e4ec24b940c2ef344e801a16fc7fe06ada6b659
wojtekl <lesniakwojciech@gmail.com>
2024-12-14 21:40:32 +0100

    update

--- pricey/webapp/DateFormatter.jsx
+++ pricey/webapp/DateFormatter.jsx
@@ -1,7 +1,7 @@
-const DateFormatter = (timestamp) => {
+const DateFormatter = (props) => {
+  const { timestamp } = props
   const locale = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(3).toLocaleLowerCase()
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
   const format = { month: "short", day: "numeric", timezone: timezone }
-  console.log(locale, timezone, format, timestamp, new Date(timestamp))
   return new Date(timestamp).toLocaleString(locale, format)
 }
