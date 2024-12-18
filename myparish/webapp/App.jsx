dfbedca3992cc01223c968a501ae15acc519c3e0
wojtekl <lesniakwojciech@gmail.com>
2024-12-18 08:34:38 +0100

    update

--- myparish/webapp/App.jsx
+++ myparish/webapp/App.jsx
@@ -13,16 +13,16 @@ class AppInner extends React.Component {
   }
 
   render() {
-    const mapDiv = React.createElement('div', { id: "map", style: { width: "100%", height: "100%"} })
-    return mapDiv
+    const mapDiv = React.createElement('div', { id: "map", style: { width: "100%", height: "300px"} })
+    return <Container fluid>{mapDiv}</Container>
   }
 
   componentDidMount() {
-    const map = L.map('map')
+    const map = L.map('map').setView([51.505, 20.09], 13)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+  }).addTo(map)
   }
 }
 
