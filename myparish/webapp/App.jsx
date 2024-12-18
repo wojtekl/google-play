cddc1834fa9143e6b2fd4edb35b9f676b1752a53
wojtekl <lesniakwojciech@gmail.com>
2024-12-18 08:28:23 +0100

    update

--- myparish/webapp/App.jsx
+++ myparish/webapp/App.jsx
@@ -14,7 +14,7 @@ class AppInner extends React.Component {
 
   render() {
     const mapDiv = React.createElement('div', { id: "map", style: { width: "100%", height: "100%"} })
-    return <Container>{mapDiv}</Container>
+    return <Container fluid>{mapDiv}</Container>
   }
 
   componentDidMount() {
