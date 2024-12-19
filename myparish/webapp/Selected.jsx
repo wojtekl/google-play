65f00e54418685af3d7b2d52bb9c98181d137b87
wojtekl <lesniakwojciech@gmail.com>
2024-12-19 21:04:44 +0100

    update

--- myparish/webapp/Selected.jsx
+++ myparish/webapp/Selected.jsx
@@ -1,7 +1,19 @@
 const Selected = () => {
   const { name } = useParams()
+  const { t } = useTranslation()
+
   const selected = clients.clients.find(i => i.name === name)
-  return (<Container>
-    <ListGroup><ListGroup.Item action href={selected.schedule}>{selected.name}</ListGroup.Item></ListGroup>
-    </Container>)
+  return (<>
+  <Navbar>
+    <Container>
+      <Navbar.Brand>{selected.name}</Navbar.Brand>
+    </Container>
+  </Navbar>
+  <Container>
+    <ListGroup><ListGroup.Item action href={selected.schedule}>{t('title_schedule')}</ListGroup.Item></ListGroup>
+    <ListGroup><ListGroup.Item action href={selected.announcement}>{t('title_announcement')}</ListGroup.Item></ListGroup>
+    <ListGroup><ListGroup.Item action href={selected.contact}>{t('title_contact')}</ListGroup.Item></ListGroup>
+    <ListGroup><ListGroup.Item action href={selected.other}>{t('title_other')}</ListGroup.Item></ListGroup>
+  </Container>
+  </>)
 }
--- myparish/webapp/resources.jsx
+++ myparish/webapp/resources.jsx
@@ -2,13 +2,21 @@ const resources = {
   en: {
     translation: {
       "title_app": "MyParish the app",
-      "see_link": "See"
+      "see_link": "See",
+      "title_schedule": "Schedule",
+      "title_announcements": "Announcements",
+      "title_contact": "Contact",
+      "title_other": "Other"
     }
   },
   pl: {
     translation: {
       "title_app": "apka Moja Parafia",
-      "see_link": "Zobacz"
+      "see_link": "Zobacz",
+      "title_schedule": "Intencje",
+      "title_announcements": "Ogłoszenia",
+      "title_contact": "Kontakt",
+      "title_other": "Cmentarz"
     }
   }
 }
