b8663730692b0606a15288ec3d9af31fb8e8fa7d
wojtekl <lesniakwojciech@gmail.com>
2024-12-19 20:32:10 +0100

    update

--- myparish/webapp/Selected.jsx
+++ myparish/webapp/Selected.jsx
@@ -1,7 +1,5 @@
-class Selected extends React.Component {
-  render() {
-    const { name } = useParams()
-    const selected = clients.clients.find(i => i.name === name)
-    return (<a href={selected.schedule}>{selected.name}</a>)
-  }
+const Selected = () => {
+  const { name } = useParams()
+  const selected = clients.clients.find(i => i.name === name)
+  return (<a href={selected.schedule}>{selected.name}</a>)
 }
