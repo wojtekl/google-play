55112bd302efaa5caca95c4eb35dff742372e900
wojtekl <lesniakwojciech@gmail.com>
2024-12-20 06:56:04 +0100

    update

--- myparish/webapp/Map.jsx
+++ myparish/webapp/Map.jsx
@@ -25,16 +25,16 @@ class MapInner extends React.Component {
   componentDidMount() {
     const { t } = this.props
 
-    const mapView = L.map('map').setView([52.114503, 19.423561], 10)
+    const map = L.map('map').setView([52.114503, 19.423561], 10)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(mapView)
+    }).addTo(map)
     clients.clients.forEach((val, index) => {
-      var marker = L.marker([val.latitude, val.longitude]).addTo(mapView);
+      var marker = L.marker([val.latitude, val.longitude]).addTo(map);
       marker.bindPopup(`<p>${val.name}</p><a href="#/selected/${val.name}"> ${t('see_link')} </a>`).openPopup();
     })
   }
 }
 
-const Map = withTranslation()(MapInner)
+const App = withTranslation()(MapInner)
