2bab52c5086aa0044c3b388002a311cab402c7d2
wojtekl <lesniakwojciech@gmail.com>
2024-12-20 08:33:24 +0100

    update

--- myparish/webapp/App.jsx
+++ myparish/webapp/App.jsx
@@ -23,8 +23,8 @@ class AppInner extends React.Component {
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#list">{t('nav_list')}</Nav.Link>
-          <Nav.Link href="#news">{t('nav_news')}</Nav.Link>
+          <Nav.Link href="#/list">{t('nav_list')}</Nav.Link>
+          <Nav.Link href="#/news">{t('nav_news')}</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
--- myparish/webapp/List.jsx
+++ myparish/webapp/List.jsx
@@ -1,5 +1,9 @@
 class List extends React.Component {
   render() {
-    return <ListGroup>{clients.clients.map(i => <ListGroup.Item onClick={alert(i.name)}>{i.name}</ListGroup.Item>)}</ListGroup>
+    return <Container>
+  <ListGroup>
+    {clients.clients.map(i => <ListGroup.Item onClick={() => alert(i.name)}>{i.name}</ListGroup.Item>)}
+  </ListGroup>
+</Container>
   }
 }
