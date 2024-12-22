93445702fe3d23640c3bab63d34911036712d3b8
wojtekl <lesniakwojciech@gmail.com>
2024-12-22 21:10:22 +0100

    update

--- myparish/webapp/App.jsx
+++ myparish/webapp/App.jsx
@@ -50,8 +50,8 @@ class AppInner extends React.Component {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map)
-    const markerRed = L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png" })
-    const markerPurple = L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png" })
+    const markerRed = L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png", iconSize: [12, 12] })
+    const markerPurple = L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png", iconSize: [12, 12] })
     clients.clients.forEach((item, index) => {
       let now = false
       item.week.forEach((val, j) => {
