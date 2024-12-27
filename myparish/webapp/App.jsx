eb862caf9f1760496d02c5c95146ebc41a0df780
wojtekl <lesniakwojciech@gmail.com>
2024-12-27 08:26:58 +0100

    update

--- myparish/webapp/App.jsx
+++ myparish/webapp/App.jsx
@@ -41,7 +41,8 @@ class AppInner extends React.Component {
         live: !!i.live,
         incoming: incoming.trim()
       }
-  })}
+    })
+  }
 
   handleSwitchActive = (event) => {
     this.setState({
@@ -56,47 +57,33 @@ class AppInner extends React.Component {
     const mapDiv = React.createElement('div', { id: "map", style: { width: "100%", height: "100%" } })
 
     return <>
-  <Navbar expand="md">
-    <Container>
-    <Navbar.Brand><img src="https://github.com/wojtekl/google-play/raw/refs/heads/main/myparish/MyParish/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp" width="30px" height="30px" />{t('title_app')}</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#/list">{t('nav_list')}</Nav.Link>
-          <Nav.Link href="#/news">{t('nav_news')}</Nav.Link>
-        </Nav>
-        <form class="form-inline my-2">
-        <div class="form-check form-switch">
-          <input type="checkbox" class="form-check-input" id="switchActive" onChange={this.handleSwitchActive} />
-          <label class="form-check-label" for="switchActive">{t('label_active')}</label>
-        </div>
-      </form>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  <Container style={{height: "calc(100vh - 59px)"}}>{mapDiv}</Container>
-</>}
+      <Navbar expand="md">
+        <Container>
+          <Navbar.Brand><img src="https://github.com/wojtekl/google-play/raw/refs/heads/main/myparish/MyParish/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp" width="30px" height="30px" />{t('title_app')}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#/list">{t('nav_list')}</Nav.Link>
+              <Nav.Link href="#/news">{t('nav_news')}</Nav.Link>
+            </Nav>
+            <form class="form-inline my-2">
+              <div class="form-check form-switch">
+                <input type="checkbox" class="form-check-input" id="switchActive" onChange={this.handleSwitchActive} />
+                <label class="form-check-label" for="switchActive">{t('label_active')}</label>
+              </div>
+            </form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container style={{ height: "calc(100vh - 59px)" }}>{mapDiv}</Container>
+    </>
+  }
 
   componentDidUpdate() {
     console.log('did update')
-    const { t } = this.props
-
-    const selected = clients.clients.find(i => i.name === store.getState().value)
-
-    this.state.map.remove()
-    const map = L.map('map').setView(selected ? [selected.latitude, selected.longitude] : [52.114503, 19.423561], 9)
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map)
-    this.state.filtered.forEach((i, _) => {
-      var marker = L.marker([i.latitude, i.longitude], { icon: !!i.incoming ? (i.live ? markerLive : markerActive) : markerDefault }).addTo(map);
-      marker.bindPopup(`<p>${i.name}</p><p>${i.incoming}</p><a href="#/selected/${i.name}"> ${t('see_link')} </a>`);
-    })
   }
 
   componentDidMount() {
-    console.log('did mount')
     const { t } = this.props
 
     const selected = clients.clients.find(i => i.name === store.getState().value)
@@ -110,10 +97,10 @@ class AppInner extends React.Component {
       map: map
     })
 
-    this.state.filtered.forEach((i, _) => {
-      var marker = L.marker([i.latitude, i.longitude], { icon: !!i.incoming ? (i.live ? markerLive : markerActive) : markerDefault }).addTo(map);
-      marker.bindPopup(`<p>${i.name}</p><p>${i.incoming}</p><a href="#/selected/${i.name}"> ${t('see_link')} </a>`);
-    })
+    const marks = L.layerGroup(this.state.filtered.map(i => L
+      .marker([i.latitude, i.longitude], { icon: !!i.incoming ? (i.live ? markerLive : markerActive) : markerDefault })
+      .bindPopup(`<p>${i.name}</p><p>${i.incoming}</p><a href="#/selected/${i.name}"> ${t('see_link')} </a>`)))
+    map.addOverlay(marks, 'parish')
   }
 }
 
