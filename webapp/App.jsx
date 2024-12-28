7a2ec9cabce642c570aa61e0606ef8130f8d6c02
wojtekl <lesniakwojciech@gmail.com>
2024-12-28 19:26:43 +0100

    update

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -1,21 +1,35 @@
-const withTranslation = ReactI18next.withTranslation
-
-const Navbar = ReactBootstrap.Navbar
-
+const withTranslation = ReactI18next.withTranslation;
 
 class AppInner extends React.Component {
   render() {
     const { t } = this.props
 
-    return <div>
-      <Navbar.Collapse id="basic-navbar-nav">
-    <Container>Teeest</Container>
-  </Navbar.Collapse>
-  <div class="navbar navbar-dark bg-dark">
-    <div class="container-fluid">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    </div>
-  </div>
+    return <div><Carousel>
+      <Carousel.Item>
+        <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel1.jpg" fluid />
+        <Carousel.Caption>
+          <h1>{t('title_offer')}</h1>
+          <p>{t('subtitle_offer')}</p>
+          <Button href="#/offer" variant="primary" size="lg">{t('button_see')}</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel2.jpg" fluid />
+        <Carousel.Caption>
+          <h1>{t('title_products')}</h1>
+          <p>{t('subtitle_products')}</p>
+          <Button href="#/products" variant="primary" size="lg">{t('button_see')}</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/webapp/carousel3.jpg" fluid />
+        <Carousel.Caption>
+          <h1>{t('title_contact')}</h1>
+          <p>{t('subtitle_contact')}</p>
+          <Button href="#/contact" variant="primary" size="lg">{t('button_see')}</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </div>
   }
 }
