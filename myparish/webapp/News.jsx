dfbce125e9248cb5ab77fb4ce77073946d8baaf9
wojtekl <lesniakwojciech@gmail.com>
2024-12-29 20:56:25 +0100

    update

--- myparish/webapp/News.jsx
+++ myparish/webapp/News.jsx
@@ -9,7 +9,7 @@ class NewsInner extends React.Component {
     return <>
       <Navbar expand="md">
         <Container>
-          <Navbar.Brand><img src="https://github.com/wojtekl/google-play/raw/refs/heads/main/myparish/MyParish/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp" width="30px" height="30px" />{t('title_app')}</Navbar.Brand>
+          <Navbar.Brand><img src="https://github.com/wojtekl/google-play/raw/refs/heads/main/myparish/MyParish/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp" width="30px" height="30px" alt="" />{t('title_app')}</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
