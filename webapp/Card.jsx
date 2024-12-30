05dc110073d8e1b52a82bbb59b6881be3a647d1a
wojtekl <lesniakwojciech@gmail.com>
2024-12-30 14:13:34 +0100

    update

---
+++ webapp/Card.jsx
@@ -0,0 +1,25 @@
+const withTranslation = ReactI18next.withTranslation;
+
+class CardInner extends React.Component {
+  render() {
+    const { image, description, urlButtonOnline, urlButtonGet, small } = this.props
+
+    return <div class="col">
+      <div class="card shadow-sm">
+        <img src={image} />
+        <div class="card-body">
+          <p class="card-text">{t(description)}</p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+            <a href={urlButtonOnline} class="btn btn-sm btn-outline-secondary" role="button">{t('button_online')}</a>
+            <a href={urlButtonGet} class="btn btn-sm btn-outline-secondary" role="button">{t('button_get')}</a>
+            </div>
+            <small class="text-body-secondary">{t(small)}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  }
+}
+
+const Card = withTranslation()(CardInner)
