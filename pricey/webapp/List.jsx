dbb285e998ca1b15511a06887ca840cdb8e9b743
wojtekl <lesniakwojciech@gmail.com>
2024-12-30 19:06:23 +0100

    update

--- pricey/webapp/List.jsx
+++ pricey/webapp/List.jsx
@@ -119,7 +119,7 @@ class ListInner extends React.Component {
                       return <td> {row[property]} </td>
                     }
                   })}
-                  {expandable && <td><button type="button" class="btn btn-sm" onClick={this.handleClick} role="button" disabled={this.state.selected !== row['id']} aria-disabled={this.state.selected !== row['id']}><Badge bg={this.state.selected === row['id'] ? 'primary' : 'secondary'}> -{'>'} </Badge></button></td>}
+                  {expandable && <td><Badge bg={this.state.selected === row['item'] ? 'primary' : 'secondary'} onClick={this.handleClick}> -{'>'} </Badge></td>}
                 </tr>)
               })}
             </tbody>
