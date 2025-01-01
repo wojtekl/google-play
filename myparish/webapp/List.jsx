6541bc94fc04c4a9759a75ae5f020026623e6c4b
wojtekl <lesniakwojciech@gmail.com>
2025-01-01 06:17:51 +0100

    update

--- myparish/webapp/List.jsx
+++ myparish/webapp/List.jsx
@@ -40,12 +40,12 @@ const List = () => {
   const filterByCriteria = (active, live, phrase) => {
     let preFiltered = all
     if (active) {
-      preFiltered = preFiltered.filter(i => !!i.incoming)
+      preFiltered = preFiltered.filter(i => !!i.incoming).sort((a, b) => a.incoming - b.incoming)
     }
     if (live) {
-      preFiltered = preFiltered.filter(i => (true === i.live) && !!i.incoming)
+      preFiltered = preFiltered.filter(i => (true === i.live) && !!i.incoming).sort((a, b) => a.incoming - b.incoming)
     }
-    setFiltered(preFiltered.filter(i => i.name.toLowerCase().includes(phrase)))
+    setFiltered(!phrase ? preFiltered : preFiltered.filter(i => i.name.toLowerCase().includes(phrase)))
   }
 
   const handleClick = (name) => {
