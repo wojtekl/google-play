eba4527bf7207390685e2c3e6921ba18a3ed3268
wojtekl <lesniakwojciech@gmail.com>
2025-01-04 14:49:32 +0100

    update

--- pricey/webapp/Modal.jsx
+++ pricey/webapp/Modal.jsx
@@ -29,32 +29,30 @@ class ModalInner extends React.Component {
             <div class="form-group">
               <label for="exampleInputName1">{t('label_name')}</label>
               <input type="text" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" name="name" value={item} required minlength="5" maxlength="100" />
-              <small id="nameHelp" class="form-text text-muted"> Upewnij się że produkt jeszcze nie istnieje </small>
+              <small id="nameHelp" class="form-text text-muted">{t('help_name')}</small>
             </div>
             <div class="form-group">
               <label for="exampleInputStore1">{t('label_store')}</label>
               <input type="text" class="form-control" id="exampleInputStore1" aria-describedby="storeHelp" name="store" value={storeName} required maxlength="50" />
-              <small id="storeHelp" class="form-text text-muted"> Sprawdź nazwę sklepu przy innych produktach </small>
+              <small id="storeHelp" class="form-text text-muted">{t('help_store')}</small>
             </div>
             <div class="form-group">
               <label for="exampleInputPrice1">{t('label_price')}</label>
               <input type="number" min="0.05" max="500" step="0.01" class="form-control" id="exampleInputPrice1" aria-describedby="priceHelp" name="price" required />
-              <small id="priceHelp" class="form-text text-muted"> Użyj kropki jako separatora </small>
+              <small id="priceHelp" class="form-text text-muted">{t('help_decimal')}</small>
             </div>
             <div class="form-group">
-              <label for="exampleInputId1"> Id </label>
               <input type="hidden" class="form-control" id="exampleInputId1" aria-describedby="idHelp" name="identyfikator" value="web" />
-              <small id="identyfikatorHelp" class="form-text text-muted"> Generowany automatycznie </small>
             </div>
             <div class="form-group form-check">
               <input type="checkbox" class="form-check-input" id="exampleInputCoupon1" aria-describedby="couponHelp" name="coupon" />
               <label for="exampleInputCoupon1" class="form-check-label">{t('label_coupon')}</label>
-              <small id="couponHelp" class="form-text text-muted"> Cena z kartą </small>
+              <small id="couponHelp" class="form-text text-muted">{t('help_membership')}</small>
             </div>
             <div class="form-group form-check">
               <input type="checkbox" class="form-check-input" id="exampleInputBulk1" aria-describedby="bulkHelp" name="bulk" />
               <label for="exampleInputBulk1" class="form-check-label">{t('label_bulk')}</label>
-              <small id="couponHelp" class="form-text text-muted"> Przy zakupie większej ilości </small>
+              <small id="couponHelp" class="form-text text-muted">{t('help_bulk')}</small>
             </div>
           </BModal.Body>
           <BModal.Footer>
