bed1206c9086e0aeb385fb5e01baa0657b9f013c
wlap.pl <wojtekl@users.noreply.github.com>
2025-05-30 09:18:11 +0200

    Update Card.jsx

--- webapp/Card.jsx
+++ webapp/Card.jsx
@@ -13,8 +13,8 @@ class CardInner extends React.Component {
           <p class="card-text">{t(description)}</p>
           <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
-              <a href={urlButtonOnline} class="btn btn-sm btn-outline-secondary" role="button" disabled={!urlButtonOnline} aria-disabled={!urlButtonOnline}>{t('button_online')}</a>
-              <a href={urlButtonGet} class="btn btn-sm btn-outline-secondary" role="button" rel="external">{t('button_get')}</a>
+              <a href={urlButtonOnline} class=`btn btn-sm btn-outline-secondary` role="button" disabled={!urlButtonOnline} aria-disabled={!urlButtonOnline}>{t('button_online')}</a>
+              <a href={urlButtonGet} class=`btn btn-sm btn-outline-secondary {!urlButtonGet && "d-none"}` role="button" rel="external">{t('button_get')}</a>
             </div>
             <small class="text-body-secondary">{t(small)}</small>
           </div>
