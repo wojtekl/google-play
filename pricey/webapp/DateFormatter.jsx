1952d7cfba54bbe447a4c27e1d635760729c72d2
wlap.pl <wojtekl@users.noreply.github.com>
2025-08-17 18:45:49 +0200

    Update DateFormatter.jsx

--- pricey/webapp/DateFormatter.jsx
+++ pricey/webapp/DateFormatter.jsx
@@ -1,7 +1,7 @@
-const DateFormatter = (props) => {
+const DateFormatter = React.memo((props) => {
   const { timestamp } = props
   const locale = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(3).toLocaleLowerCase()
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
   const format = { month: "short", day: "numeric", timezone: timezone }
   return new Date(timestamp).toLocaleString(locale, format)
-}
+})
