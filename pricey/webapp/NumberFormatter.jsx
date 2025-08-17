8d39957358259dcbc227c2bcbaa1c36f458451e1
wlap.pl <wojtekl@users.noreply.github.com>
2025-08-17 18:46:26 +0200

    Update NumberFormatter.jsx

--- pricey/webapp/NumberFormatter.jsx
+++ pricey/webapp/NumberFormatter.jsx
@@ -1,6 +1,6 @@
-const NumberFormatter = (props) => {
+const NumberFormatter = React.memo((props) => {
   const { value } = props
   const locale = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(3).toLocaleLowerCase()
   const format = { maximumFractionDigits: 2, minimumFractionDigits: 2 }
   return value.toLocaleString(locale, format)
-}
+})
