6d974104415d599b8a2335de15ced269839cf9e6
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-10 04:05:29 +0100

    Update main.jsx

--- pricey/webapp/main.jsx
+++ pricey/webapp/main.jsx
@@ -44,8 +44,8 @@ i18next.use(initReactI18next).init({
   }
 })
 
-const storeName = new URLSearchParams(new URL(window.location).search).get('store') ?? null
-const day = new URLSearchParams(new URL(window.location).search).get('day') ?? 0
+const storeName = new URLSearchParams(new URL(window.location).search).get('store') ?? undefined
+const day = new URLSearchParams(new URL(window.location).search).get('day') ?? undefined
 
 const store = Redux.createStore(selectedReducer)
 store.subscribe(() => { localStorage.setItem('redux', JSON.stringify(store.getState())) })
