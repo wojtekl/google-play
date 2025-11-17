786b6114cdb03fb643cacd4a4fdaa582c60851d8
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-17 13:18:09 +0100

    Update Hero.jsx

--- webapp/Hero.jsx
+++ webapp/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   }, [name])
   
   return <div class="px-4 py-5 my-5 text-center">
-      <img class="d-block mx-auto mb-4" src={image}>
+      <img class="d-block mx-auto mb-4" src={image} />
       <h1 class="display-5 fw-bold">{t(small)}</h1>
       <div class="col-lg-6 mx-auto">
         <p class="lead m-4">{t(description)}</p>
