788b68e49f1cad35508ffc2b010e881c8e8f001a
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-17 13:58:36 +0100

    Update App.jsx

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -53,20 +53,21 @@ class AppInner extends React.Component {
             </div>
           </div>
         </section>
-
+        <Divider />
         <Hero
           image="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/pricey/Pricey/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp"
           description="description_pricey"
           urlButtonOnline="https://cennik.wlap.pl/"
           small="name_pricey"
         />
-        <div style={{width: '100%', height: '3rem', backgroundColor: '#0000001a', border: 'solid rgba(0,0,0,.15)', borderWidth: '1px 0', boxShadow: 'inset 0 .5em 1.5em #0000001a,inset 0 .125em .5em #00000026'}}></div>
+        <Divider />
         <Hero
           image="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MojaParafia/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp"
           description="description_myparish"
           urlButtonOnline="https://parafia.wlap.pl/"
           small="name_myparish"
         />
+        <Divider />
       </main>
 
       <footer class="text-body-secondary py-5">
