ffd087c3c8e20c1f39b38afcea3fccc69001a4cd
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-17 14:57:25 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -2,7 +2,9 @@ const Manage = () => {
   return <div class="container-fluid">
     <div class="row">
       <div class="sidebar border border-right col-md-3 com-lg-2 p-0 bg-body-tertiary"></div>
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"></main>
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <h2>Section Title</h2>
+      </main>
     </div>
   </div>
 }
