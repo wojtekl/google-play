cfc5214c42ae37f2414ffcdcd595d0a903fc936e
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-17 13:54:06 +0100

    Update App.jsx

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -60,7 +60,7 @@ class AppInner extends React.Component {
           urlButtonOnline="https://cennik.wlap.pl/"
           small="name_pricey"
         />
-
+        <div style={{width: '100%', height: '3rem', backgroundColor: '#0000001a', border: 'solid rgba(0,0,0,.15)', borderWidth: '1px 0', boxShadow: 'inset 0 .5em 1.5em #0000001a,inset 0 .125em .5em #00000026'}}></div>
         <Hero
           image="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MojaParafia/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp"
           description="description_myparish"
