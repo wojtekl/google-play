bd1c9d6f30a1bf861655a355c3f84e1bcaa79045
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-18 12:39:21 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -32,7 +32,16 @@ const Manage = () => {
         </div>
       </div>
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 class="h2">Dashboard</h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group me-2">
+              <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            </div>
+          </div>
+        </div>
         <h2>Section Title</h2>
+        <div class="table-responsive small"></div>
       </main>
     </div>
   </div>
