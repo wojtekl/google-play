ca915fa852bfbc4484ed993380c8313d40c295cd
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-18 12:23:22 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -1,7 +1,36 @@
 const Manage = () => {
   return <div class="container-fluid">
     <div class="row">
-      <div class="sidebar border border-right col-md-3 com-lg-2 p-0 bg-body-tertiary"></div>
+      <div class="sidebar border border-right col-md-3 com-lg-2 p-0 bg-body-tertiary">
+        <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="sidebarMenuLabel">Nazwa Parafii</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#"> Dashboard </a>
+              </li>
+            </ul>
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+              <span> Saved reports</span>
+              <a class="link-secondary" href="#" aira-label="Add a new report"></a>
+            </h6>
+            <ul class="nav flex-column mb-auto">
+              <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2" href="#">Current month</a>
+              </li>
+            </ul>
+            <hr class="my-3" />
+            <ul class="nav flex-column mb-auto">
+              <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2" href="#">Settings</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <h2>Section Title</h2>
       </main>
