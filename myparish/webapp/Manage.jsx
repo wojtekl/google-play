904db94359cfcf7dffdd76dba0a27d6bcba2406c
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-18 12:48:13 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -1,5 +1,14 @@
 const Manage = () => {
-  return <div class="container-fluid">
+  return <>
+    <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Nazwa parafii</a>
+      <ul class="navbar-nav flex-row d-md-none">
+        <li class="nav-item text-nowrap">
+          <button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search"></button>
+        </li>
+      </ul>
+    </header>
+    <div class="container-fluid">
     <div class="row">
       <div class="sidebar border border-right col-md-3 com-lg-2 p-0 bg-body-tertiary">
         <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
@@ -44,5 +53,5 @@ const Manage = () => {
         <div class="table-responsive small"></div>
       </main>
     </div>
-  </div>
+  </div></>
 }
