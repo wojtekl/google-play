526f66193d9bcae2cd560346b01987ecf1427a96
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-19 15:02:58 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -24,11 +24,10 @@ const Manage = () => {
     <div class="row">
       <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
         <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="sidebarMenuLabel">Nazwa Parafii</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
-          </div>
-          <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Nazwa Parafii</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
             <ul class="nav flex-column">
               <li class="nav-item">
                 <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" onClick={switchTab} id="dashboardBT"> Dashboard </a>
@@ -49,7 +48,7 @@ const Manage = () => {
                 <a class="nav-link d-flex align-items-center gap-2" onClick={switchTab} id="settingsBT">Settings</a>
               </li>
             </ul>
-          </div>
+          </Offcanvas.Body>
         </div>
       </div>
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
