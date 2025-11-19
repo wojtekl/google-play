84136095f6279e24e1cf27e5de8ab2e325344184
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-19 12:43:56 +0100

    Create Tab1.jsx

---
+++ myparish/webapp/Tab1.jsx
@@ -0,0 +1,12 @@
+const Tab1 = () => {
+  return <><div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 class="h2">Dashboard</h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group me-2">
+              <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            </div>
+          </div>
+        </div>
+        <h2>Section Title</h2>
+        <div class="table-responsive small"></div></>
+}
