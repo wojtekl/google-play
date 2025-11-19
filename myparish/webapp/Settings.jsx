6905ff4816f5c7f1cfc0f5f380a0239cdc7761e9
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-19 16:29:52 +0100

    Create Settings.jsx

---
+++ myparish/webapp/Settings.jsx
@@ -0,0 +1,29 @@
+const Settings = () => {
+  return <>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">Settings</h1>
+      <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group me-2">
+          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+        </div>
+      </div>
+    </div>
+    <h2>Section Title</h2>
+    <div class="table-responsive small">
+      <table class="table table-stripped table-sm">
+        <thead>
+          <tr>
+            <td>Head1</td>
+            <td>Head2</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Col1</td>
+            <td>Col1</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </>
+}
