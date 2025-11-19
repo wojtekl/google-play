94b0cdb83e82b743acc4042024a7bdddfe764bf0
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-19 14:13:36 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -16,7 +16,7 @@ const Manage = () => {
       <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Nazwa parafii</a>
       <ul class="navbar-nav flex-row d-md-none">
         <li class="nav-item text-nowrap">
-          <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">menu</button>
+          <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation"><i class="bi bi-list" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu"></i></button>
         </li>
       </ul>
     </header>
