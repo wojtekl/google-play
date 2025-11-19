b74ecd89b7a1440bc19e6bfde794ef00b269b92b
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-19 14:25:11 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -23,7 +23,7 @@ const Manage = () => {
     <div class="container-fluid">
     <div class="row">
       <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-        <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+        <div class="offcanvas offcanvas-start bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
           <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="sidebarMenuLabel">Nazwa Parafii</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
