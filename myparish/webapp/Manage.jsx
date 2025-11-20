d8c5eab29a624752ea394a9c8241be20209570ef
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-20 20:03:34 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -1,4 +1,5 @@
 const Manage = () => {
+  const { t } = useTranslation()
   const [selectedTab, setSelectedTab] = useState('dashboardBT')
 
   const switchTab = (e) => {
@@ -26,31 +27,31 @@ const Manage = () => {
       <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
         <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="sidebarMenuLabel">Nazwa Parafii</h5>
+            <h5 class="offcanvas-title" id="sidebarMenuLabel">{t('label_nazwa')}</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" onClick={switchTab} id="dashboardBT"><i class="bi bi-house-fill"></i> Dashboard </a>
+                <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" onClick={switchTab} id="dashboardBT"><i class="bi bi-house-fill"></i> {t('label_dashboard')} </a>
               </li>
             </ul>
             <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-              <span> Saved reports </span>
-              <button class="link-secondary" aira-label="Add a new report" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-plus-circle"></i></button>
+              <span> {t('label_scheduled_event')} </span>
+              <button class="link-secondary" aria-label="Add a new report" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-plus-circle"></i></button>
             </h6>
             <ul class="nav flex-column mb-auto">
               <li class="nav-item">
-                <a class="nav-link d-flex align-items-center gap-2" onClick={switchTab} id="currentMonthBT"><i class="bi bi-file-earmark-text"></i> Current month </a>
+                <a class="nav-link d-flex align-items-center gap-2" onClick={switchTab} id="currentMonthBT"><i class="bi bi-file-earmark-text"></i> {t('label_current_week')} </a>
               </li>
             </ul>
             <hr class="my-3" />
             <ul class="nav flex-column mb-auto">
               <li class="nav-item">
-                <a class="nav-link d-flex align-items-center gap-2" onClick={switchTab} id="settingsBT"><i class="bi bi-gear-wide-connected"></i> Settings </a>
+                <a class="nav-link d-flex align-items-center gap-2" onClick={switchTab} id="settingsBT"><i class="bi bi-gear-wide-connected"></i> {t('label_settings')} </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link d-flex align-items-center gap-2" onClick={() => {alert('logout')}} id="signout"><i class="bi bi-door-closed"></i> Sign out </a>
+                <a class="nav-link d-flex align-items-center gap-2" onClick={() => {alert('logout')}} id="signout"><i class="bi bi-door-closed"></i> {t('label_signout')} </a>
               </li>
             </ul>
           </div>
