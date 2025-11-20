b2b8bb666593af18de50ffeb4cd7ac2a595919e0
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-20 19:42:13 +0100

    Update Modal.jsx

--- myparish/webapp/Modal.jsx
+++ myparish/webapp/Modal.jsx
@@ -1,14 +1,13 @@
 
 
 const Modal = () => {
+  const { t } = useTranslation()
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    //const { handleClose } = this.props
     const form = document.querySelector('#form_item')
-    axios.post(`scheduledCreate`, form, { headers: { 'Content-Type': 'multipart/form-data' }}).then(() => {
+    axios.post(`api/scheduled-put`, form, { headers: { 'Content-Type': 'multipart/form-data' }}).then(() => {
       form.reset()
-      //handleClose()
     })
     return false
   }
@@ -17,7 +16,7 @@ const Modal = () => {
         <div class="modal-dialog modal-dialog-centered modal-sm">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">{"newProduct"}</h5>
+              <h5 class="modal-title" id="exampleModalLabel">{t('label_scheduled')}</h5>
               <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -25,22 +24,22 @@ const Modal = () => {
             <div class="modal-body">
               <form class="dane" id="form_item">
                 <div class="form-group">
-                  <label for="exampleInputNazwa1">{"name"}</label>
-                  <input type="text" class="form-control" id="exampleInputNazwa1" aria-describedby="nazwaHelp" name="description" value={""} />
-                  <small id="nazwaHelp" class="form-text text-muted">Upewnij się że produkt jeszcze nie istnieje</small>
+                  <label for="exampleInputDescription1">{t('label_description')}</label>
+                  <input type="text" class="form-control" id="exampleInputDescription1" aria-describedby="descriptionHelp" name="description" />
+                  <small id="descriptionHelp" class="form-text text-muted">Upewnij się że produkt jeszcze nie istnieje</small>
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputSklep1">{"store"}</label>
-                  <input type="text" class="form-control" id="exampleInputSklep1" aria-describedby="sklepHelp" name="scheduled" />
-                  <small id="sklepHelp" class="form-text text-muted">Sprawdź nazwę sklepu przy innych produktach</small>
+                  <label for="exampleInputScheduled1">{t('label_scheduled')}</label>
+                  <input type="text" class="form-control" id="exampleInputScheduled1" aria-describedby="schedulehHelp" name="scheduled" />
+                  <small id="scheduledHelp" class="form-text text-muted">Sprawdź nazwę sklepu przy innych produktach</small>
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputCena1">{"price"}</label>
-                  <input type="text" class="form-control" id="exampleInputCena1" aria-describedby="cenaHelp" name="value" />
-                  <small id="cenaHelp" class="form-text text-muted">Użyj kropki jako separatora</small>
+                  <label for="exampleInputValue1">{t('label_value')}</label>
+                  <input type="text" class="form-control" id="exampleInputValue1" aria-describedby="valueHelp" name="value" />
+                  <small id="valueHelp" class="form-text text-muted">Użyj kropki jako separatora</small>
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputNotes1">{"notes"}</label>
+                  <label for="exampleInputNotes1">{t('notes')}</label>
                   <input type="text" class="form-control" id="exampleInputNotes1" aria-describedby="notesHelp" name="notes" />
                   <small id="notesHelp" class="form-text text-muted">Sprawdź nazwę sklepu przy innych produktach</small>
                 </div>
@@ -53,11 +52,6 @@ const Modal = () => {
                   <label for="exampleInputCoupon1">{"coupon"}</label>
                   <input type="checkbox" class="form-control" id="exampleInputCoupon1" aria-describedby="couponHelp" name="coupon" />
                   <small id="couponHelp" class="form-text text-muted">Cena z kartą</small>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputBulk1">{"bulk"}</label>
-                  <input type="checkbox" class="form-control" id="exampleInputBulk1" aria-describedby="bulkHelp" name="bulk" />
-                  <small id="couponHelp" class="form-text text-muted">Przy zakupie większej ilości</small>
                 </div>
               </form>
             </div>
