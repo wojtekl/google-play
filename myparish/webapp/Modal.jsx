6f5e7e4dacc7d8ceb7cee8c0d78111425b34d6a9
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-20 14:20:43 +0100

    Update Modal.jsx

--- myparish/webapp/Modal.jsx
+++ myparish/webapp/Modal.jsx
@@ -17,7 +17,7 @@ const Modal = () => {
         <div class="modal-dialog modal-dialog-centered modal-sm">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">{localise.newProduct}</h5>
+              <h5 class="modal-title" id="exampleModalLabel">{"newProduct"}</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -25,17 +25,17 @@ const Modal = () => {
             <div class="modal-body">
               <form class="dane">
                 <div class="form-group">
-                  <label for="exampleInputNazwa1">{localise.name}</label>
+                  <label for="exampleInputNazwa1">{"name"}</label>
                   <input type="text" class="form-control" id="exampleInputNazwa1" aria-describedby="nazwaHelp" name="nazwa" value={""} />
                   <small id="nazwaHelp" class="form-text text-muted">Upewnij się że produkt jeszcze nie istnieje</small>
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputSklep1">{localise.store}</label>
+                  <label for="exampleInputSklep1">{"store"}</label>
                   <input type="text" class="form-control" id="exampleInputSklep1" aria-describedby="sklepHelp" name="sklep" />
                   <small id="sklepHelp" class="form-text text-muted">Sprawdź nazwę sklepu przy innych produktach</small>
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputCena1">{localise.price}</label>
+                  <label for="exampleInputCena1">{"price"}</label>
                   <input type="text" class="form-control" id="exampleInputCena1" aria-describedby="cenaHelp" name="cena" />
                   <small id="cenaHelp" class="form-text text-muted">Użyj kropki jako separatora</small>
                 </div>
@@ -45,20 +45,20 @@ const Modal = () => {
                   <small id="identyfikatorHelp" class="form-text text-muted">Generowany automatycznie</small>
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputCoupon1">{localise.coupon}</label>
+                  <label for="exampleInputCoupon1">{"coupon"}</label>
                   <input type="checkbox" class="form-control" id="exampleInputCoupon1" aria-describedby="couponHelp" name="coupon" />
                   <small id="couponHelp" class="form-text text-muted">Cena z kartą</small>
                 </div>
                 <div class="form-group">
-                  <label for="exampleInputBulk1">{localise.bulk}</label>
+                  <label for="exampleInputBulk1">{"bulk"}</label>
                   <input type="checkbox" class="form-control" id="exampleInputBulk1" aria-describedby="bulkHelp" name="bulk" />
                   <small id="couponHelp" class="form-text text-muted">Przy zakupie większej ilości</small>
                 </div>
               </form>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">{localise.cancel}</button>
-              <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={handleSubmit}>{localise.save}</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">{"cancel"}</button>
+              <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={handleSubmit}>{"save"}</button>
             </div>
           </div>
         </div>
