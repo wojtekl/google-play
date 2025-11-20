48a7b8b4ff03bb931671d18966f8bdf52b7de869
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-20 14:26:48 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -37,7 +37,7 @@ const Manage = () => {
             </ul>
             <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
               <span> Saved reports </span>
-              <a class="link-secondary" href="#" aira-label="Add a new report" data-toggle="modal" data-target="#exampleModal"><i class="bi bi-plus-circle"></i></a>
+              <a class="link-secondary" href="" aira-label="Add a new report" data-toggle="modal" data-target="#exampleModal"><i class="bi bi-plus-circle"></i></a>
             </h6>
             <ul class="nav flex-column mb-auto">
               <li class="nav-item">
