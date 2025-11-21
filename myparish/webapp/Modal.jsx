6f8db452b493003ca95de09b93749df3cbc9371c
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-21 07:07:00 +0100

    Update Modal.jsx

--- myparish/webapp/Modal.jsx
+++ myparish/webapp/Modal.jsx
@@ -30,7 +30,7 @@ const Modal = () => {
                 </div>
                 <div class="form-group">
                   <label for="exampleInputScheduled1">{t('label_scheduled')}</label>
-                  <input type="date" class="form-control" id="exampleInputScheduled1" aria-describedby="schedulehHelp" name="scheduled" />
+                  <input type="datetime-local" class="form-control" id="exampleInputScheduled1" aria-describedby="schedulehHelp" name="scheduled" />
                   <small id="scheduledHelp" class="form-text text-muted">Sprawdź nazwę sklepu przy innych produktach</small>
                 </div>
                 <div class="form-group">
