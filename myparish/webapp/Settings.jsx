fed3296cba5aefd2f5b399374c5c5a6a7847a05b
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-22 13:56:09 +0100

    Update Settings.jsx

--- myparish/webapp/Settings.jsx
+++ myparish/webapp/Settings.jsx
@@ -6,7 +6,7 @@ const Settings = () => {
       <h1 class="h2">{t('label_settings')}</h1>
       <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group me-2">
-          <button type="button" class="btn btn-sm btn-outline-secondary">{t('label_edit)}</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">{t('label_edit')}</button>
         </div>
       </div>
     </div>
