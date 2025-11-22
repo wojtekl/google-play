7c9ca6e14f4f5927112239b0e434a1fccd698ae4
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-22 12:47:54 +0100

    Update Dashboard.jsx

--- myparish/webapp/Dashboard.jsx
+++ myparish/webapp/Dashboard.jsx
@@ -1,4 +1,6 @@
 const Dashboard = () => {
+  const { t } = useTranslation()
+  
   return <>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2">{t('label_dashboard')}</h1>
