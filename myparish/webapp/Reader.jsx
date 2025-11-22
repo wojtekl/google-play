7b2b45f584c13c3675faa792eda5413fe0f9715a
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-22 15:16:22 +0100

    Update Reader.jsx

--- myparish/webapp/Reader.jsx
+++ myparish/webapp/Reader.jsx
@@ -1,39 +1,46 @@
-const useEffect = React.useEffect
 const useParams = ReactRouterDOM.useParams
+const useEffect = React.useEffect
 
 const Reader = () => {
   const { t } = useTranslation()
-  const [scheduledWeek, setScheduledWeek] = useState([])
+  const { tenant } = useParams()
+  const [currentWeek, setCurrentWeek] = useState([])
 
-  const { name } = useParams()
   useEffect(() => {
-      const postData = {
-        type: "msza",
-        tenant: `${name}`
-      }
-      axios.post(`api/scheduled-week`, postData, { headers: { 'Content-Type': 'multipart/form-data' }}).then((response) => setScheduledWeek(response.data))
-    }, [name])
-  
-  return <><h2>{name}</h2><div class="table-responsive small">
+    const postData = {
+      type: "msza",
+      tenant: `${tenant}`
+    }
+    axios.post('api/scheduled-week', postData, { headers: { 'Content-Type': 'multipart/form-data' }}).then((response) => {
+      setCurrentWeek(response.data)
+    })
+  }, [tenant])
+
+  return <>
+    <h2>{`${t('label_tenant')}: ${tenant}`}</h2>
+    <div class="table-responsive small">
       <table class="table table-stripped table-sm">
         <thead>
           <tr>
-            <td>{t('label_description')}</td>
-            <td>{t('label_scheduled')}</td>
-            <td>{t('label_value')}</td>
-            <td>{t('label_notes')}</td>
+            <th scope="col">#</th>
+            <th scope="col">{t('label_description')}</th>
+            <th scope="col">{t('label_scheduled')}</th>
+            <th scope="col">{t('label_value')}</th>
+            <th scope="col">{t('label_notes')}</th>
           </tr>
         </thead>
         <tbody>
-          {scheduledWeek.map(s => {
+          {currentWeek.map((e, i) => {
             return <tr>
-              <td>{s['description']}</td>
-              <td>{s['scheduled']}</td>
-              <td>{s['value']}</td>
-              <td>{s['notes']}</td>
+              <td>{i}</td>
+              <td>{e['description']}</td>
+              <td><DateFormatter timestamp={e['scheduled']} /></td>
+              <td><NumberFormatter value={e['value']} /></td>
+              <td>{e['notes']}</td>
             </tr>
           })}
         </tbody>
       </table>
-    </div></>
+    </div>
+  </>
 }
