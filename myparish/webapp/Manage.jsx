b359329bc9dcb487b32553cd9581b0374d627ee3
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-24 14:09:41 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -91,7 +91,7 @@ const Manage = () => {
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
           <DisplayTab />
         </main>
-        <Modal modalId="newScheduledModal" />
+        <Modal modalId="newScheduledModal" key="newScheduledModal" />
       </div>
     </div>
   </>
