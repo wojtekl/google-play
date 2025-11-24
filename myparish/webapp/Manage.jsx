98bbf9e1b1909b922627e8809b1c1805d9e773b6
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-24 15:47:32 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -67,7 +67,7 @@ const Manage = () => {
               </ul>
               <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
                 <span>{t('label_scheduled_event')}</span>
-                <a class="link-secondary" href="#" aria-label={t('label_add_scheduled')} data-bs-toggle="modal" data-bs-target="#modalId">
+                <a class="link-secondary" href="#" aria-label={t('label_add_scheduled')} data-bs-toggle="modal" data-bs-target="#exampleModal">
                   <i class="bi bi-plus-circle"></i>
                 </a>
               </h6>
