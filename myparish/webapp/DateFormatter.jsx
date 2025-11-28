8e97c885c8e1d79a28704529a602f9311ce4c259
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-28 13:20:03 +0100

    Update DateFormatter.jsx

--- myparish/webapp/DateFormatter.jsx
+++ myparish/webapp/DateFormatter.jsx
@@ -2,6 +2,6 @@ const DateFormatter = React.memo((props) => {
   const { timestamp } = props
   const locale = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(3).toLocaleLowerCase()
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-  const format = { month: "short", day: "numeric", timezone: timezone }
+  const format = { month: "short", day: "numeric", hour: "numeric", minute: "numeric", timezone: timezone }
   return new Date(timestamp).toLocaleString(locale, format)
 })
