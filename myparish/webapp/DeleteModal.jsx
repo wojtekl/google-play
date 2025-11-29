7c71819aed7cf14405b11b7cb44357b62b8f06e3
wlap.pl <wojtekl@users.noreply.github.com>
2025-11-29 14:25:34 +0100

    Update DeleteModal.jsx

--- myparish/webapp/DeleteModal.jsx
+++ myparish/webapp/DeleteModal.jsx
@@ -1,3 +1,22 @@
 const DeleteModal = () => {
-  return <div></div>
+  const { t } = useTranslation()
+  const { onOK } = props
+  
+  return <div class="modal" id="deleteModal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{t('label_delete_title')}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label={t('label_close')}></button>
+        </div>
+        <div class="modal-body">
+          <p>{t('label_delete')}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{t('label_cancel')}</button>
+          <button type="button" class="btn btn-primary" onClick={onOk}>{t('label_ok')}</button>
+        </div>
+      </div>
+    </div>
+  </div>
 }
