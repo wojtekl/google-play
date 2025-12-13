ed73d7745a4c7ff752e4bc085b7275641a162883
wlap.pl <wojtekl@users.noreply.github.com>
2025-12-13 15:09:47 +0100

    Create Weeks.jsx

---
+++ myparish/webapp/Weeks.jsx
@@ -0,0 +1,44 @@
+const Weeks = () => {
+  const { t } = useTranslation()
+
+  const currentYear = new Date()
+  currentYear.setHours(0, 0, 0, 0)
+  currentYear.setMonth(0)
+  currentYear.setDay(1)
+  currentYear.setDate(currentYear.getDate() - currentYear.getDay())
+  const weeks = new Array()
+  while(currentYear.getFullYear() == 2025) {
+    weeks.push(currentYear.getUTCDate())
+    currentYear.setDate(currentYear.getDate() + 7)
+  }
+
+  return <>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">{t('label_weeks')}</h1>
+      <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group me-2">
+          <button type="button" class="btn btn-sm btn-outline-secondary">{t('label_edit')}</button>
+        </div>
+      </div>
+    </div>
+    <h2>{t('label_section')}</h2>
+    <div class="table-responsive small">
+      <table class="table table-stripped table-sm">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">{t('label_header')}</th>
+          </tr>
+        </thead>
+        <tbody>{weeks.map(w => 
+        <tr>
+            <td>1</td>
+            <td>{w}</td>
+          </tr>
+          )
+        }
+        </tbody>
+      </table>
+    </div>
+  </>
+}
