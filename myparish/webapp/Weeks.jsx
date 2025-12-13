c0b4b02e33c97322fea7e1444f4c8caa4f172e2e
wlap.pl <wojtekl@users.noreply.github.com>
2025-12-13 16:07:47 +0100

    Update Weeks.jsx

--- myparish/webapp/Weeks.jsx
+++ myparish/webapp/Weeks.jsx
@@ -1,5 +1,6 @@
 const Weeks = () => {
   const { t } = useTranslation()
+  const [selectedWeek, setSelectedWeek] = useState()
 
   const months = [t('label_january'), t('label_february'), t('label_march'), t('label_april'), t('label_may'), t('label_june'), t('label_july'), t('label_august'), t('label_september'), t('label_october'), t('label_november'), t('label_december')]
 
@@ -13,13 +14,13 @@ const Weeks = () => {
   const weeks = new Array()
   while(currentYear.getFullYear() < nextYear) {
     weeks.push({
-      start: currentYear.toISOString(),
+      start: currentYear.toISOString().split('T')[0],
       month: months[currentYear.getMonth()]
     })
     currentYear.setDate(currentYear.getDate() + 7)
   }
 
-  return <>
+  return selectedWeek ? <CurrentWeek date={selectedWeek} /> : <>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2">{t('label_weeks')}</h1>
       <div class="btn-toolbar mb-2 mb-md-0">
@@ -36,6 +37,7 @@ const Weeks = () => {
             <th scope="col">#</th>
             <th scope="col">{t('label_header')}</th>
             <th scope="col">{t('label_month')}</th>
+            <th scope="col">{t('label_actions')}</th>
           </tr>
         </thead>
         <tbody>{weeks.map(w => 
@@ -43,6 +45,7 @@ const Weeks = () => {
             <td>1</td>
             <td><DateFormatter timestamp={w.start} /></td>
             <td>{w.month}</td>
+            <td><button type="button" class="btn btn-sm btn-outline-secondary" onClick={() => { setSelectedWeek(w.start) }}><i class="bi bi-pencil-square"></i></button></td>
           </tr>
           )
         }
