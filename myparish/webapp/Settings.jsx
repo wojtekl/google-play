6868a106401b6464d0a5286faa8b749725dabd32
wlap.pl <wojtekl@users.noreply.github.com>
2025-12-14 16:05:14 +0100

    Update Settings.jsx

--- myparish/webapp/Settings.jsx
+++ myparish/webapp/Settings.jsx
@@ -1,31 +1,51 @@
 const Settings = () => {
   const { t } = useTranslation()
 
+  const [settings, setSettings] = useState()
+  const [disabled, setDisabled] = useState(true)
+
+  const handleDisabled = () => {
+    setDisabled(!disabled)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const form = document.querySelector(`#form_settings`)
+    
+    axios.post('api/settings', form, { headers: { 'Content-Type': 'multipart/form-data' }}).then((response) => {
+      console.debug(response.data)
+    })
+    
+    return false
+  }
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams()
+    searchParams.append('tenant', tenant)
+    axios.get(`api/settings?${searchParams.toString()}`).then((response) => {
+      setSettings(response.data)
+      document.getElementById('settingsSchedule').value = response.data.schedule
+    })
+  }, [tenant])
+
   return <>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2">{t('label_settings')}</h1>
       <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group me-2">
-          <button type="button" class="btn btn-sm btn-outline-secondary">{t('label_edit')}</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary" onClick={handleDisabled}>{disabled ? <i class="bi bi-unlock"></i> : <i class="bi bi-lock"></i>}</button>
         </div>
       </div>
     </div>
-    <h2>{t('label_section')}</h2>
-    <div class="table-responsive small">
-      <table class="table table-stripped table-sm">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">{t('label_header')}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>value</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <form id="form_settings">
+      <fieldset disabled={disabled}>
+        <legend>{t('label_settings')}</legend>
+        <div class="mb-3">
+          <label for="settingsSchedule" class="form-label">{t('label_schedule')}</label>
+          <input type="text" id="settingsSchedule" class="form-control" placeholder={settings?.schedule} name="schedule" />
+        </div>
+        {!disabled && <button type="submit" class="btn btn-primary" onClick={handleSubmit}>{t('label_submit')}</button>}
+      </fieldset>
+    </form>
   </>
 }
