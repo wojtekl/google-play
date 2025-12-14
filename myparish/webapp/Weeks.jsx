6ff69a02dcd0bef32850fa3f7a772661e726a3cc
wlap.pl <wojtekl@users.noreply.github.com>
2025-12-14 13:59:15 +0100

    Update Weeks.jsx

--- myparish/webapp/Weeks.jsx
+++ myparish/webapp/Weeks.jsx
@@ -25,7 +25,7 @@ const Weeks = () => {
   }
 
   return selectedWeek ? <>
-    <button type="button" class="btn btn-sm btn-outline-secondary" onClick={ () => { setSelectedWeek(undefined) }}>{t('label_powrot')}</button>
+    <button type="button" class="btn btn-sm btn-outline-secondary" onClick={ () => { setSelectedWeek(undefined) }}>{t('label_back')}</button>
     <CurrentWeek date={selectedWeek} type={'eucharystia'} />
   </> : <>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
