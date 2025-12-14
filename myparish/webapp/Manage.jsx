675995783d9af2f67354167f072f5e760d85e15d
wlap.pl <wojtekl@users.noreply.github.com>
2025-12-14 13:36:23 +0100

    Update Manage.jsx

--- myparish/webapp/Manage.jsx
+++ myparish/webapp/Manage.jsx
@@ -61,7 +61,7 @@ const Manage = () => {
     return <Dashboard />
   }
   
-  return <>
+  return !tenant ? <></> : <>
     <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
       <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">{`${t('label_tenant')}: ${tenant}`}</a>
       <ul class="navbar-nav flex-row d-md-none">
