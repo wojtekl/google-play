a5cc3103a154ee2319bac8e6e2a94e63305c872c
wlap.pl <wojtekl@users.noreply.github.com>
2025-12-17 15:49:09 +0100

    Update ConfirmModal.jsx

--- myparish/webapp/ConfirmModal.jsx
+++ myparish/webapp/ConfirmModal.jsx
@@ -1,16 +1,16 @@
 const ConfirmModal = (props) => {
   const { t } = useTranslation()
-  const { onOk } = props
+  const { title, onOk } = props
   
   return <div class="modal" id="confirmModal" tabindex="-1">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">{t('label_delete_title')}</h5>
+          <h5 class="modal-title">{t('label_title_confirm')}</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label={t('label_close')}></button>
         </div>
         <div class="modal-body">
-          <p>{t('label_delete')}</p>
+          <p>{t(title)}</p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{t('label_cancel')}</button>
