50a52e8db28b6f74a0c21b9f9874ecb3f7e793d0
wlap.pl <wojtekl@users.noreply.github.com>
2025-12-21 14:19:03 +0100

    Update Modal.jsx

--- myparish/webapp/Modal.jsx
+++ myparish/webapp/Modal.jsx
@@ -45,30 +45,30 @@ const Modal = (props) => {
             <div class="form-group">
               <label for={`${modalId}InputDescription`}>{t('label_description')}</label>
               <input type="text" class="form-control" id={`${modalId}InputDescription`} aria-describedby={`${modalId}HelpDescription`} name="description" />
-              <small id={`${modalId}HelpDescription`} class="form-text text-muted">{t('label_help_description')}</small>
+              <small id={`${modalId}HelpDescription`} class="form-text text-muted">{t('help_description')}</small>
             </div>
             <div class="form-group">
               <label for={`${modalId}InputScheduled`}>{t('label_date')}</label>
               <input type="datetime-local" class="form-control" id={`${modalId}InputScheduled`} aria-describedby={`${modalId}HelpScheduled`} name="scheduled" />
-              <small id={`${modalId}HelpScheduled`} class="form-text text-muted">{t('label_help_scheduled')}</small>
+              <small id={`${modalId}HelpScheduled`} class="form-text text-muted">{t('help_scheduled')}</small>
             </div>
             <div class="form-group">
               <label for={`${modalId}InputValue`}>{t('label_donation')}</label>
               <input type="number" min="10.00" max="500" step="0.01" class="form-control" id={`${modalId}InputValue`} aria-describedby={`${modalId}valueHelp`} name="value" />
-              <small id={`${modalId}valueHelp`} class="form-text text-muted">{t('label_help_value')}</small>
+              <small id={`${modalId}valueHelp`} class="form-text text-muted">{t('help_value')}</small>
             </div>
             <div class="form-group">
               <label for={`${modalId}InputNotes`}>{t('label_notes')}</label>
               <input type="text" class="form-control" id={`${modalId}InputNotes`} aria-describedby={`${modalId}HelpNotes`} name="notes" />
-              <small id={`${modalId}HelpNotes`} class="form-text text-muted">{t('label_help_notes')}</small>
+              <small id={`${modalId}HelpNotes`} class="form-text text-muted">{t('help_notes')}</small>
             </div>
             <div class="form-group">
               <input type="hidden" class="form-control" id={`${modalId}InputId`} aria-describedby={`${modalId}HelpId`} name="id" />
-              <small id={`${modalId}HelpId`} class="form-text text-muted">{t('label_help_id')}</small>
+              <small id={`${modalId}HelpId`} class="form-text text-muted">{t('help_id')}</small>
             </div>
             <div class="form-group">
               <input type="hidden" class="form-control" id={`${modalId}InputType`} aria-describedby={`${modalId}HelpType`} name="type" />
-              <small id={`${modalId}HelpType`} class="form-text text-muted">{t('label_help_type')}</small>
+              <small id={`${modalId}HelpType`} class="form-text text-muted">{t('help_type')}</small>
             </div>
           </form>
         </div>
