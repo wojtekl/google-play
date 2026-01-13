71e8943ffbf4b016b00acb418f440202ae3e2850
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-13 14:19:04 +0100

    Update Settings.jsx

--- myparish/webapp/Settings.jsx
+++ myparish/webapp/Settings.jsx
@@ -16,6 +16,7 @@ const Settings = () => {
     const form = {
       schedule: document.getElementById('settingsSchedule').value,
       showVisits: document.getElementById('settingsShowVisits').checked ? 1 : 0
+      showBooking: document.getElementById('settingsShowBooking').checked ? 1 : 0
     }
     axios.post('api/settings', form, { headers: { 'Content-Type': 'multipart/form-data' }}).then((response) => {
       console.debug(response.data)
@@ -31,6 +32,7 @@ const Settings = () => {
       setSettings(response.data)
       document.getElementById('settingsSchedule').value = response.data.schedule
       document.getElementById('settingsShowVisits').checked = 0 == response.data.showVisits ? false : true
+      document.getElementById('settingsShowBooking').checked = 0 == response.data.showBooking ? false : true
     })
   }, [tenant])
 
@@ -53,6 +55,10 @@ const Settings = () => {
         <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="settingsShowVisits" name="showVisits" />
           <label class="form-check-label" for="settingsShowVisits">{t('label_show_visit')}</label>
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="settingsShowBooking" name="showBooking" />
+          <label class="form-check-label" for="settingsShowBooking">{t('label_show_booking')}</label>
         </div>
         {!disabled && <button type="submit" class="btn btn-primary" onClick={handleSubmit}>{t('label_submit')}</button>}
       </fieldset>
