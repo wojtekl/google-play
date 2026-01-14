bd158a029e1fdf8203b123b4d073f5abeea27859
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-14 14:03:00 +0100

    Update News.jsx

--- myparish/webapp/News.jsx
+++ myparish/webapp/News.jsx
@@ -22,9 +22,8 @@ class NewsInner extends React.PureComponent {
               {selected && <Nav.Link href={`#/selected/${selected.name}`}>{t('nav_your')}</Nav.Link>}
               <Nav.Link href="#/">{t('nav_map')}</Nav.Link>
               <Nav.Link href="#/list">{t('nav_list')}</Nav.Link>
-              <Nav.Link href="https://wlap.pl/" rel="author">{t('nav_aboutus')}</Nav.Link>
-              <Nav.Link href={t('url_privacy')} rel="privacy-policy">{t('nav_privacy')}</Nav.Link>
               <Nav.Link onClick={handleInstall}>{t('nav_install')}</Nav.Link>
+              <Nav.Link href="#/manage">{t('nav_manage')}</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -35,6 +34,8 @@ class NewsInner extends React.PureComponent {
           <ListGroup.Item action href="https://www.gosc.pl/mobile" rel="external">Gość Niedzielny</ListGroup.Item>
           <ListGroup.Item action href="https://rycerzniepokalanej.pl/" rel="external">Rycerz Niepokalanej</ListGroup.Item>
           <ListGroup.Item action href="https://biblia.deon.pl/" rel="external">Biblia Tysiąclecia</ListGroup.Item>
+          <ListGroup.Item action href={t('url_privacy')} rel="privacy-policy">{t('nav_privacy')}</ListGroup.Item>
+          <ListGroup.Item action href="https://wlap.pl/" rel="author">{t('nav_aboutus')}</ListGroup.Item>
           <ListGroup.Item action href="https://cennik.wlap.pl/" rel="external">Historia cen produktów spożywczych w marketach</ListGroup.Item>
         </ListGroup>
       </Container>
