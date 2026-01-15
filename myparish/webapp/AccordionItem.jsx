918ffd9cbff3472b9784640e1540460ae46c42fc
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-15 14:42:07 +0100

    Update AccordionItem.jsx

--- myparish/webapp/AccordionItem.jsx
+++ myparish/webapp/AccordionItem.jsx
@@ -1,24 +1,17 @@
-const withTranslation = ReactI18next.withTranslation
+const AccordionItem = React.memo((props) => {
+  const { t } = useTranslation()
+  const { id, parent, show = false, children } = props
 
-
-class AccordionItemInner extends React.Component {
-
-  render() {
-    const { id, parent, show = false, children, t } = this.props
-
-    return <div class="accordion-item">
-      <h2 class="accordion-header">
-        <button class={`accordion-button ${!show ? 'collapsed' : ''}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${id}`} aria-expanded="true" aria-controlls={`collapse${id}`}>{t(`label_${id}`)}</button>
-      </h2>
-      <div id={`collapse${id}`} class={`accordion-collapse collapse ${!!show ? 'show' : ''}`} data-bs-parent={`#${parent}`}>
-        <div class="accordion-body">
-          <div class="row">
-            {children}
-          </div>
+  return <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class={`accordion-button ${!show ? 'collapsed' : ''}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${id}`} aria-expanded="true" aria-controlls={`collapse${id}`}>{t(`label_${id}`)}</button>
+    </h2>
+    <div id={`collapse${id}`} class={`accordion-collapse collapse ${!!show ? 'show' : ''}`} data-bs-parent={`#${parent}`}>
+      <div class="accordion-body">
+        <div class="row">
+          {children}
         </div>
       </div>
     </div>
-  }
-}
-
-const AccordionItem = withTranslation()(AccordionItemInner)
+  </div>
+})
