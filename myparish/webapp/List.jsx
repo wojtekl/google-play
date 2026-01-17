f60068169bd17553a5d63e98b036d2bd8e53f7fa
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-17 15:39:34 +0100

    Update List.jsx

--- myparish/webapp/List.jsx
+++ myparish/webapp/List.jsx
@@ -1,33 +1,9 @@
 const List = React.memo(() => {
   const navigate = useNavigate()
   const { t } = useTranslation()
-
-  /*const dayOfMonth = new Date().getDate()
-  const month = new Date().getMonth() + 1
-  const isSunday = (0 === new Date().getDay()) || (1 === month && (1 === dayOfMonth || 6 === dayOfMonth)) || (11 === month && 1 === dayOfMonth) || (12 === month && (25 === dayOfMonth || 26 === dayOfMonth)) ? true : false
-
-  const all = clients.clients.map(i => {
-    let incoming = ''
-    const now = new Date()
-    const base = new Date()
-    const schedule = isSunday ? (i.sunday ?? []) : i.week
-    schedule.forEach((j, _) => {
-      base.setHours(j.substring(0, 2))
-      base.setMinutes(j.substring(3, 5))
-      const diff = base - now
-      if ((diff >= -(1000 * 60 * 5)) && (diff < (1000 * 60 * 60))) {
-        incoming = `${incoming} ${j}`
-      }
-    })
-    return {
-      name: i.name,
-      live: !!i.live,
-      incoming: incoming.trim()
-    }
-  })*/
   const all = useGetClients()
 
-  const selected = clients.clients.find(i => i.name === store.getState().value)
+  //const selected = clients.clients.find(i => i.name === store.getState().value)
 
   const [filtered, setFiltered] = useState(all)
   const [phrase, setPhrase] = useState('')
