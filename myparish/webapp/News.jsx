e2ca38fbe5ed75b2f52acbdef2acfdba8150b7c4
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-18 17:07:23 +0100

    Update News.jsx

--- myparish/webapp/News.jsx
+++ myparish/webapp/News.jsx
@@ -1,20 +1,18 @@
 const News = React.memo((props) => {
   const { t } = useTranslation()
   
-  //const selected = clients.clients.find(i => i.name === store.getState().value)
-  
   return <>
     <Navi current="news" />
     <div class="container">
-      <ListGroup>
-        <ListGroup.Item action href="https://m.niedziela.pl/" rel="external">Niedziela</ListGroup.Item>
-        <ListGroup.Item action href="https://www.gosc.pl/mobile" rel="external">Gość Niedzielny</ListGroup.Item>
-        <ListGroup.Item action href="https://rycerzniepokalanej.pl/" rel="external">Rycerz Niepokalanej</ListGroup.Item>
-        <ListGroup.Item action href="https://biblia.deon.pl/" rel="external">Biblia Tysiąclecia</ListGroup.Item>
-        <ListGroup.Item action href={t('url_privacy')} rel="privacy-policy">{t('nav_privacy')}</ListGroup.Item>
-        <ListGroup.Item action href="https://wlap.pl/" rel="author">{t('nav_aboutus')}</ListGroup.Item>
-        <ListGroup.Item action href="https://cennik.wlap.pl/" rel="external">Historia cen produktów spożywczych w marketach</ListGroup.Item>
-      </ListGroup>
+      <div class="list-group">
+        <a href="https://m.niedziela.pl/" rel="external" class="list-group-item list-group-item-action">Niedziela</a>
+        <a href="https://www.gosc.pl/mobile" rel="external" class="list-group-item list-group-item-action">Gość Niedzielny</a>
+        <a href="https://rycerzniepokalanej.pl/" rel="external" class="list-group-item list-group-item-action">Rycerz Niepokalanej</a>
+        <a href="https://biblia.deon.pl/" rel="external" class="list-group-item list-group-item-action">Biblia Tysiąclecia</a>
+        <a href={t('url_privacy')} rel="privacy-policy" class="list-group-item list-group-item-action">{t('nav_privacy')}</a>
+        <a href="https://wlap.pl/" rel="author" class="list-group-item list-group-item-action">{t('nav_aboutus')}</a>
+        <a href="https://cennik.wlap.pl/" rel="external" class="list-group-item list-group-item-action">Historia cen produktów spożywczych w marketach</a>
+      </div>
     </div>
   </>
 })
