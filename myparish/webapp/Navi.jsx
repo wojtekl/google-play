57a3ff5e93c2c0ff1a30c34b7ccc5c86fd6e2d59
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-19 13:57:33 +0100

    Update Navi.jsx

--- myparish/webapp/Navi.jsx
+++ myparish/webapp/Navi.jsx
@@ -19,21 +19,23 @@ const Navi = React.memo((props) => {
   }
 
   return <>
-    <Navbar expand="md">
+    <div class="navbar navbar-expand-md">
       <div class="container">
-        <Navbar.Brand><img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MojaParafia/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp" width="30px" height="30px" alt="" />{t('title_app')}</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <div class="navbar-brand"><img src="https://raw.githubusercontent.com/wojtekl/google-play/refs/heads/main/myparish/MojaParafia/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp" width="30px" height="30px" alt="" />{t('title_app')}</div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#basic-navbar-nav" aria-controls="basic-navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="basic-navbar-nav">
+          <div className="navbar-nav me-auto">
             {selected && 'selected' != current && <Nav.Link href={`#/selected/${selected.name}`}>{t('nav_your')}</Nav.Link>}
             {'map' != current && <Nav.Link href="#/">{t('nav_map')}</Nav.Link>}
             {'list' != current && <Nav.Link href="#/list">{t('nav_list')}</Nav.Link>}
             {'news' != current && <Nav.Link href="#/news">{t('nav_news')}</Nav.Link>}
             <Nav.Link onClick={handleInstall}>{t('nav_install')}</Nav.Link>
             <Nav.Link href="#/manage">{t('nav_manage')}</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          </div>
+        </div>
       </div>
-    </Navbar>
+    </div>
   </>
 })
