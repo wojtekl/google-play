0adf7a1357ae0ca8a81edc4932f3b4a5d9d51c00
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-23 14:59:41 +0100

    Update App.jsx

--- myparish/webapp/App.jsx
+++ myparish/webapp/App.jsx
@@ -2,6 +2,8 @@ const App = React.memo(() => {
   const { t } = useTranslation()
   const [selected, setSelected] = useState(clients.clients.find(i => i.name === store.getState().value))
 
+  const list = React.useMemo(useGetClients())
+
   useEffect(() => {
     const map = L.map('map').setView(selected ? [selected.latitude, selected.longitude] : [52.114503, 19.423561], 9)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,8 +14,7 @@ const App = React.memo(() => {
     const markerDefault = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px" aria-label="Marker default"></i>', className: "markerDefault", size: [20, 23], iconAnchor: [10, 11] })
     const markerLive = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px; color: red" aria-label="Marker live"></i>', className: "markerLive", size: [20, 23], iconAnchor: [10, 11] })
     const markerActive = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px; color: blue" aria-label="Marker active"></i>', className: "markerActive", size: [20, 23], iconAnchor: [10, 11] })
-
-    const list = useGetClients()
+    
     const inactive = L.layerGroup(list.filter(i => !i.incoming).map(i => L
       .marker([i.latitude, i.longitude], { icon: markerDefault })
       .bindPopup(`<p>${i.name}</p><p>${i.incoming}</p><a href="#/selected/${i.name}"> ${t('see_link')} </a>`))).addTo(map)
@@ -26,7 +27,7 @@ const App = React.memo(() => {
     document.title = t('title_app')
     document.getElementsByTagName('meta').description.content = t('meta_description')
     document.getElementsByTagName('meta').keywords.content = t('meta_keywords')
-  }, [])
+  }, [list])
 
   const mapDiv = React.createElement('div', { id: "map", style: { width: "100%", height: "100%" } })
 
