b51e30501717f024a6f9af8ade4d1a8a28d623c5
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-23 14:50:03 +0100

    Update App.jsx

--- myparish/webapp/App.jsx
+++ myparish/webapp/App.jsx
@@ -1,37 +1,19 @@
-const withTranslation = ReactI18next.withTranslation
+const App = React.memo(() => {
+  const { t } = useTranslation()
+  const [selected, setSelected] = useState(clients.clients.find(i => i.name === store.getState().value))
 
-
-class AppInner extends React.PureComponent {
-
-  state = {
-    selected: clients.clients.find(i => i.name === store.getState().value)
-  }
-
-  render() {
-    const { t } = this.props
-
-    const { selected } = this.state
-
-    const mapDiv = React.createElement('div', { id: "map", style: { width: "100%", height: "100%" } })
-
-    return <>
-      <Navi current="map" />
-      <div class="container" style={{ height: "calc(100vh - 59px)" }}>{mapDiv}</div>
-    </>
-  }
-
-  componentDidMount() {
-    const { t } = this.props
-
-    const { selected } = this.state
-
+  useEffect(() => {
     const map = L.map('map').setView(selected ? [selected.latitude, selected.longitude] : [52.114503, 19.423561], 9)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright" rel="external">OpenStreetMap</a>'
     }).addTo(map)
 
-    const list = useGetClients()
+    const markerDefault = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px" aria-label="Marker default"></i>', className: "markerDefault", size: [20, 23], iconAnchor: [10, 11] })
+    const markerLive = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px; color: red" aria-label="Marker live"></i>', className: "markerLive", size: [20, 23], iconAnchor: [10, 11] })
+    const markerActive = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px; color: blue" aria-label="Marker active"></i>', className: "markerActive", size: [20, 23], iconAnchor: [10, 11] })
+
+    const list = React.useMemo(useGetClients())
     const inactive = L.layerGroup(list.filter(i => !i.incoming).map(i => L
       .marker([i.latitude, i.longitude], { icon: markerDefault })
       .bindPopup(`<p>${i.name}</p><p>${i.incoming}</p><a href="#/selected/${i.name}"> ${t('see_link')} </a>`))).addTo(map)
@@ -44,11 +26,12 @@ class AppInner extends React.PureComponent {
     document.title = t('title_app')
     document.getElementsByTagName('meta').description.content = t('meta_description')
     document.getElementsByTagName('meta').keywords.content = t('meta_keywords')
-  }
-}
+  }, [])
 
-const App = withTranslation()(AppInner)
+  const mapDiv = React.createElement('div', { id: "map", style: { width: "100%", height: "100%" } })
 
-const markerDefault = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px" aria-label="Marker default"></i>', className: "markerDefault", size: [20, 23], iconAnchor: [10, 11] })
-const markerLive = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px; color: red" aria-label="Marker live"></i>', className: "markerLive", size: [20, 23], iconAnchor: [10, 11] })
-const markerActive = L.divIcon({ html: '<i class="bi bi-geo-alt-fill" style="font-size: 20px; color: blue" aria-label="Marker active"></i>', className: "markerActive", size: [20, 23], iconAnchor: [10, 11] })
+  return <>
+    <Navi current="map" />
+    <div class="container" style={{ height: "calc(100vh - 59px)" }}>{mapDiv}</div>
+  </>
+})
