e4133db4c3a94d7f92de64a61aa90b5bcbffaf84
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-23 15:46:42 +0100

    Update App.jsx

--- pricey/webapp/App.jsx
+++ pricey/webapp/App.jsx
@@ -1,35 +1,30 @@
-const withTranslation = ReactI18next.withTranslation
-
 const Spinner = ReactBootstrap.Spinner
 
 
-class AppInner extends React.PureComponent {
+const App = React.memo(() => {
+  const { t } = useTranslation()
+  
+  const [source, setSource] = useState(<Container fluid>
+    <Row className="mt-3">
+      <Spinner animation="border" variant="warning" role="status" />
+    </Row>
+  </Container>)
+  const [warning, setWarning] = useState(store.getState().warning)
 
-  state = {
-    source: (
-      <Container fluid>
-        <Row className="mt-3">
-          <Spinner animation="border" variant="warning" role="status" />
-        </Row>
-      </Container>
-    ),
-    warning: store.getState().warning
+  const handleReplace = (source) => {
+    setSource(source)
   }
 
-  handleReplace = (source) => {
-    this.setState({ source: source })
+  const handleBack = () => {
+    setSource(<App />)
   }
 
-  handleBack = () => {
-    this.setState({ source: <App /> })
-  }
-
-  handleGotit = () => {
-    this.setState({ warning: false })
+  const handleGotit = () => {
+    setWarning(false)
     store.dispatch({ type: 'warning/set' })
   }
 
-  componentDidMount() {
+  useEffect(() => {
     const searchParams = new URLSearchParams()
     searchParams.append('lang', store.getState().lang)
     const selected = new URLSearchParams(new URL(window.location).search).get('selected')
@@ -38,28 +33,21 @@ class AppInner extends React.PureComponent {
     }
     const parent = this
     axios.get(`items?${searchParams.toString()}`).then(function (response) {
-      parent.handleReplace(<List properties={columns_list} list={response.data} expandable={true} replace={parent.handleReplace} back={parent.handleBack} />)
+      handleReplace(<List properties={columns_list} list={response.data} expandable={true} replace={handleReplace} back={handleBack} />)
     })
 
-    const { t } = this.props
     document.title = t('title_app')
     document.getElementsByTagName('meta').description.content = t('meta_description')
     document.getElementsByTagName('meta').keywords.content = t('meta_keywords')
-  }
+  }, [store])
 
-  render() {
-    const { t } = this.props
-    const { warning, source } = this.state
-    return !warning ? source : <div class="px-4 py-5 my-5 text-center">
-      <h1 class="display-5 fw-bold"></h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="lead m-4">{t('message_warning')}</p>
-        <div class="d-grip gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3" onClick={this.handleGotit}>{t('button_gotit')}</button>
-        </div>
+  return !warning ? source : <div class="px-4 py-5 my-5 text-center">
+    <h1 class="display-5 fw-bold"></h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead m-4">{t('message_warning')}</p>
+      <div class="d-grip gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3" onClick={handleGotit}>{t('button_gotit')}</button>
       </div>
     </div>
-  }
-}
-
-const App = withTranslation()(AppInner)
+  </div>
+})
