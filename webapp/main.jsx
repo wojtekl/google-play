c3e639714a3c7db6a4579ef961360cbc9f4a0b11
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-24 14:23:25 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import * as i18n from 'i18next'
-import reactI18next from 'react-i18next'
 import { resources } from './resources'
 //const Router = ReactRouterDOM.BrowserRouter
 //const Routes = ReactRouterDOM.Routes
 //const Route = ReactRouterDOM.Route
 
-//const initReactI18next = ReactI18next.initReactI18next
+const initReactI18next = ReactI18next.initReactI18next
 
 
 const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
 
 console.log(i18n)
 console.log(reactI18next)
-i18n.use(initReactI18next).init({
+i18next.use(initReactI18next).init({
   resources: resources,
   lng: lang,
   fallbacking: "pl",
