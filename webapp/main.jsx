933e4a3581f4cb441da1638b44f6e1e545cb9091
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-24 15:02:06 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -10,12 +10,12 @@ import { default as initReactI18next } from 'https://esm.sh/react-i18next'
 
 //const initReactI18next = ReactI18next.initReactI18next
 
-console.log(i18n)
+console.log(i18next)
 console.log(initReactI18next)
 
 const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
 
-i18n.use(initReactI18next).init({
+i18next.use(initReactI18next).init({
   resources: resources,
   lng: lang,
   fallbacking: "pl",
