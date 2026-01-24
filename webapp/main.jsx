68d7ed79d63b4710bd0cbfc829c853ef4a13c0de
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-24 21:58:06 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 //import { resources } from './resources'
-//import * as i18next from 'https://unpkg.com/i18next/dist/esm/i18next.js'
-//import * as initReactI18next from 'react-i18next2/*'
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 //const Router = ReactRouterDOM.BrowserRouter
 //const Routes = ReactRouterDOM.Routes
 //const Route = ReactRouterDOM.Route
 
 //const initReactI18next = ReactI18next.initReactI18next
 
-alert(React)
+alert(Router)
 
 /*const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
 
@@ -24,13 +24,10 @@ i18next.use(initReactI18next).init({
 })*/
 
 render((
- <p>okejka</p>
-), document.getElementById('app'))
-/*
  <Router>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/howto" element={<Howto />} />
+      <Route path="/" element={<p>raz</p>} />
+      <Route path="/howto" element={<p>dwa</p>} />
     </Routes>
   </Router>
-  */
+), document.getElementById('app'))
