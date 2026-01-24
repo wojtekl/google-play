15470efa43171799310ce0ece3bbe1136c48459c
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-24 14:38:11 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -3,17 +3,18 @@ import { render } from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { resources } from './resources'
 import i18n from 'https://esm.sh/i18next'
+import { initReactI18next } from 'https://esm.sh/react-i18next'
 //const Router = ReactRouterDOM.BrowserRouter
 //const Routes = ReactRouterDOM.Routes
 //const Route = ReactRouterDOM.Route
 
-const initReactI18next = ReactI18next.initReactI18next
+//const initReactI18next = ReactI18next.initReactI18next
 
 console.log(i18n)
 
 const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
 
-i18next.use(initReactI18next).init({
+i18n.use(initReactI18next).init({
   resources: resources,
   lng: lang,
   fallbacking: "pl",
