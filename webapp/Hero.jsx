ed50c4ff7e4acda02e393a146fcb12064d170ebe
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-24 12:27:52 +0100

    Update Hero.jsx

--- webapp/Hero.jsx
+++ webapp/Hero.jsx
@@ -1,5 +1,5 @@
-const useNavigate = ReactRouterDOM.useNavigate
-const useTranslation = ReactI18next.useTranslation
+import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 const Hero = (props) => {
   const { image, description, urlButtonOnline, small } = props
