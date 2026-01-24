d321a03723f53569cf00df73dcef9a03ac4223c7
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-24 21:46:33 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { resources } from './resources'
-import * as i18next from 'https://unpkg.com/i18next/dist/esm/i18next.js'
-import * as initReactI18next from 'react-i18next2/*'
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+//import { resources } from './resources'
+//import * as i18next from 'https://unpkg.com/i18next/dist/esm/i18next.js'
+//import * as initReactI18next from 'react-i18next2/*'
 //const Router = ReactRouterDOM.BrowserRouter
 //const Routes = ReactRouterDOM.Routes
 //const Route = ReactRouterDOM.Route
 
 //const initReactI18next = ReactI18next.initReactI18next
 
-console.log(i18next)
-console.log(initReactI18next)
+alert(react)
 
-const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
+/*const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
 
 i18next.use(initReactI18next).init({
   resources: resources,
@@ -22,13 +21,16 @@ i18next.use(initReactI18next).init({
   interpolation: {
     escapeValue: false
   }
-})
+})*/
 
 render((
-  <Router>
+ <p>okejka</p>
+), document.getElementById('app'))
+/*
+ <Router>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/howto" element={<Howto />} />
     </Routes>
   </Router>
-), document.getElementById('app'))
+  */
