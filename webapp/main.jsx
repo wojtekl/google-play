3fc6b39dc6cdd0895af010f6a87150cbc573f97c
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-24 12:03:36 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { render } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 //const Router = ReactRouterDOM.BrowserRouter
