32c72a026d7ccda1a4085f14920f0bcf7c6f8fa2
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-25 10:31:21 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import App from '/App'
+import App from 'https://raw.githubusercontent.com/wojtekl/google-play/main/webapp/App.mjs'
 //const Router = ReactRouterDOM.BrowserRouter
 //const Routes = ReactRouterDOM.Routes
 //const Route = ReactRouterDOM.Route
