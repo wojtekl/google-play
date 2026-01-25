1a4ed514a7c5abf42dde4bbbbc7b64401516e57a
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-25 14:15:51 +0100

    Update Hero.jsx

--- webapp/Hero.jsx
+++ webapp/Hero.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 //const useNavigate = (await import('react-router-dom')).useNavigate
