cf5614391fe4d9bdd38efd7d6b140f0e6110af3d
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-25 09:36:26 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -22,8 +22,10 @@ i18n.use(initReactI18next).init({
 })
 
 render((
-  <>
-    okejka2
- 
-  </>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/howto" element={<Howto />} />
+    </Routes>
+  </Router>
 ), document.getElementById('app'))
