db5c1f1347246075c9acae7027a16898cede8595
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-25 14:35:45 +0100

    Update main.jsx

--- webapp/main.jsx
+++ webapp/main.jsx
@@ -9,9 +9,7 @@ import { initReactI18next } from 'react-i18next'
 
 //const initReactI18next = ReactI18next.initReactI18next
 
-console.log(globalThis.App)
-
-while(!globalThis.App || !globalThis.Howto) {}
+console.log(App)
 
 const lang = new URLSearchParams(new URL(window.location).search).get('lang') ?? navigator.language.substring(0, 2).toLocaleLowerCase()
 
