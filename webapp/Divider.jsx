6a326b103fa22679b311b8f04df38891b803d63e
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-25 14:37:05 +0100

    Update Divider.jsx

--- webapp/Divider.jsx
+++ webapp/Divider.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-const Divider = () => {
+var Divider = () => {
   return <div style={{width: '100%', height: '3rem', backgroundColor: '#0000001a', border: 'solid rgba(0,0,0,.15)', borderWidth: '1px 0', boxShadow: 'inset 0 .5em 1.5em #0000001a,inset 0 .125em .5em #00000026'}}></div>
 }
-globalThis.Divider = Divider
 export default Divider
