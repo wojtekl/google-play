d80e5b5e909fec0967ec7b461a6b1cf8f38a0697
wlap.pl <wojtekl@users.noreply.github.com>
2026-01-25 12:25:11 +0100

    Update App.jsx

--- webapp/App.jsx
+++ webapp/App.jsx
@@ -1,6 +1,6 @@
 //import React from 'react'
 //import { withTranslation } from 'react-i18next'
-React = await import('react')
+const React = await import('react')
 //const withTranslation = import('react-i18next').withTranslation
 
 
